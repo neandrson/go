91f89f724f9a44b2cc238512b98a5fe0532111af
neandrson <a2361@yandex.ru>
2024-10-24 16:16:22 +0300

    Update findminmaxInarray_main.go

--- FindMinMaxInArray/findminmaxInarray_main.go
+++ FindMinMaxInArray/findminmaxInarray_main.go
@@ -6,13 +6,31 @@ import (
 	//"strings"
 )
 
-func ThirdElementInArray(array [6]int) int {
-	//var numb [6]int = [6]array
+func FindMinMaxInArray(array [10]int) (int, int) {
+	var num int
+	var nummin []int = []int{array[0]}
+	var nummax []int = []int{array[0]}
+	var j int
+
+	for i := 0; i >= 10; i++ {
+		if num > array[i] {
+			fmt.Println(num, array[i])
+			nummax[i] = num
+			j++
+		} else if num < array[i] {
+			nummin[i] = num
+			j++
+		} else {
+			nummin[i] = 0
+			j++
+		}
+	}
+
 	//fmt.Println(array[2])
-	return array[2]
+	return nummin[j], nummax[j]
 }
 
 func main() {
-	var num [6]int = [6]int{6, 5, 4, 3, 2, 1}
-	fmt.Println(ThirdElementInArray(num))
+	var num [10]int = [10]int{5, 2, 6, 9, 6, 5, 4, 3, 2, 1}
+	fmt.Println(FindMinMaxInArray(num))
 }

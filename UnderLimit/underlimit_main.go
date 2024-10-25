cfbf62a912abb65db42e7b102a8283f804def521
neandrson <a2361@yandex.ru>
2024-10-25 19:20:37 +0300

    Update underlimit_main.go

--- UnderLimit/underlimit_main.go
+++ UnderLimit/underlimit_main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func UnderLimit(nums []int, limit int, n int) ([]int, error) {
+	a := nums[:]
+	//var err errors
+	if len(a) < n; limit, err = int.Pars {
+		return a[:limit], errors.ErrUnsupported
+	}
+	return a[:n], nil
 
 }
 
@@ -12,8 +19,9 @@ func main() {
 	var b, c int
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	fmt.Scan("Введите длину и объём массива ", &b, &c)
-	d, err := fmt.Println(UnderLimit(a, b, c))
+	fmt.Scan("Введите общую и количество возращаемого массива ", &b, &c)
+	
+	d := make([]int, b)
 	if err != nil {
 		fmt.Println("Превышен объём")
 	}

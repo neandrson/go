313203b6b27db7ad49a6abe272c51291738c59ce
neandrson <a2361@yandex.ru>
2024-10-25 09:43:22 +0300

    Update prettyarrayoutput_main.go

--- PrettyArrayOutput/prettyarrayoutput_main.go
+++ PrettyArrayOutput/prettyarrayoutput_main.go
@@ -7,30 +7,31 @@ import (
 )
 
 func PrettyArrayOutput(array [9]string) {
-	var nummin, nummax int
+	var j int
 
 	for i := 0; i <= 8; i++ {
-		fmt.Println(array[i])
-		if array[i] == 0 {
-			fmt.Println(array[i])
-			nummin = 0
-		} else if nummax < array[i] {
-			fmt.Println(nummax)
-			nummax = array[i]
+		//fmt.Println(array[i])
+		if i <= 6 {
+			j++
+			fmt.Println(j, "я уже сделал:", array[i])
 		} else {
-			nummin = array[i]
-			fmt.Println(nummin)
-
+			j++
+			fmt.Println(j, "не успел сделать:", array[i])
 		}
 	}
-
-	//fmt.Println(array[2])
-	return nummax, nummin
 }
 
 func main() {
-	input := [9]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	max, min := FindMinMaxInArray(input)
-
-	fmt.Print(max, min)
+	input := [9]string{
+		"проснуться",
+		"позавтракать",
+		"сходить в школу",
+		"пообедать",
+		"погулять с друзьями",
+		"сделать домашнюю работу",
+		"попрограммировать на Go",
+		"поужинать",
+		"лечь спать",
+	}
+	PrettyArrayOutput(input)
 }

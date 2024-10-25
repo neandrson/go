86b0aa36d1c3bd972cd59ab5342fbbc030b4fe19
neandrson <a2361@yandex.ru>
2024-10-25 08:21:36 +0300

    Create prettyarrayoutput_main.go

---
+++ PrettyArrayOutput/prettyarrayoutput_main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	//"strconv"
+	//"strings"
+)
+
+func FindMinMaxInArray(array [10]int) (int, int) {
+	var nummin, nummax int
+
+	for i := 0; i <= 9; i++ {
+		fmt.Println(array[i])
+		if array[i] == 0 {
+			fmt.Println(array[i])
+			nummin = 0
+		} else if nummax < array[i] {
+			fmt.Println(nummax)
+			nummax = array[i]
+		} else {
+			nummin = array[i]
+			fmt.Println(nummin)
+
+		}
+	}
+
+	//fmt.Println(array[2])
+	return nummax, nummin
+}
+
+func main() {
+	input := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	max, min := FindMinMaxInArray(input)
+
+	fmt.Print(max, min)
+}

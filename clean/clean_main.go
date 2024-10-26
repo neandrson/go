2d3535932199f7b19c62a861fe26deb805f4fe90
neandrson <a2361@yandex.ru>
2024-10-26 19:37:28 +0300

    Update clean_main.go

--- clean/clean_main.go
+++ clean/clean_main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"testing"
 )
 
-func UnderLimit(nums []int, limit int, n int) ([]int, error) {
+func Clean(nums []int, x int) []int {
 	// Проверка на отрицательное значение n
 	if n < 0 {
 		return nil, errors.New("n cannot be negative")
@@ -23,66 +22,6 @@ func UnderLimit(nums []int, limit int, n int) ([]int, error) {
 		}
 	}
 	return result, nil
-}
-
-func TestUnderLimit(t *testing.T) {
-	type test struct {
-		nums      []int
-		n         int
-		limit     int
-		expected  []int
-		wantError bool
-	}
-
-	tests := []test{
-		{
-			nums:      []int{4, 7, 89, 3, 21, 2, 5, 7, 32, 4, 6, 8, 0, 3, 4, 6, 2, 115, 12},
-			n:         5,
-			limit:     3,
-			expected:  []int{2, 0, 2},
-			wantError: false,
-		},
-		{
-			nums:      nil,
-			wantError: true,
-		},
-		{
-			nums:      []int{},
-			n:         5,
-			limit:     3,
-			expected:  []int{},
-			wantError: false,
-		},
-		{
-			nums:      []int{3, 5, 6},
-			n:         5,
-			limit:     10,
-			expected:  []int{3, 5, 6},
-			wantError: false,
-		},
-		{
-			nums:      []int{-13, 0, 6},
-			n:         1,
-			limit:     -5,
-			expected:  []int{-13},
-			wantError: false,
-		},
-		{
-			nums:      []int{},
-			n:         -1,
-			limit:     5,
-			wantError: true,
-		},
-	}
-
-	for _, tc := range tests {
-		result, err := UnderLimit(tc.nums, tc.limit, tc.n)
-		if tc.wantError {
-			if err == nil {
-				t.Fatalf("n cannot be negative")
-			}
-		}
-	}
 }
 
 func main() {

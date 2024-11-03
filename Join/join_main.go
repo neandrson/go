deb5bb1d94a063c7924197fe4b546767c4db9576
neandrson <a2361@yandex.ru>
2024-11-03 18:45:00 +0300

    Update join_main.go

--- Join/join_main.go
+++ Join/join_main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func Join(nums1, nums2 []int) []int {
-	a := make([]int, len(nums1))
-	b := make([]int, len(nums2))
-	fmt.Println(len(a), cap(a), len(b), cap(b))
-	c := append(a, b...)
-	fmt.Println(len(a), len(b))
+	a := make([]int, len(nums1)+len(nums2))
+	a = append(nums1, nums2...)
+	copy(a, a[:len(nums1)+len(nums2)])
+	//a = a[len(nums1)+len(nums2) : len(nums1)+len(nums2)]
+	fmt.Println(len(a), cap(a), a)
 
-	return c
+	return a
 }
 
 /*func main() {

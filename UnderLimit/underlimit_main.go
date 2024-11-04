68e0006fde65ca293c6d4205bcfdb56b7abf1f78
neandrson <a2361@yandex.ru>
2024-11-04 17:36:41 +0300

    Update underlimit_main.go

--- UnderLimit/underlimit_main.go
+++ UnderLimit/underlimit_main.go
@@ -2,26 +2,9 @@ package main
 
 import (
 	"errors"
-	"fmt"
 )
 
 func UnderLimit(nums []int, limit int, n int) ([]int, error) {
-	/*// Проверка на отрицательное значение n
-	if n < 0 {
-		return nil, errors.New("n cannot be negative")
-	}
-	// Инициализация результирующего слайса
-	result := []int{}
-	// Перебор элементов в слайсе
-	for _, num := range nums {
-		if num < limit {
-			result = append(result, num)
-			if len(result) == n {
-				break
-			}
-		}
-	}
-	return result, nil*/
 	// Проверка на отрицательное значение n
 	if n < 0 {
 		return nil, errors.New("n cannot be negative")
@@ -37,11 +20,11 @@ func UnderLimit(nums []int, limit int, n int) ([]int, error) {
 			}
 		}
 	}
-
 	return result, nil
+
 }
 
-func main() {
+/*func main() {
 	nums := []int{5, 3, 10, 7, 2, 8}
 	limit := 6
 	n := 3
@@ -52,4 +35,4 @@ func main() {
 	} else {
 		fmt.Println("Result:", result)
 	}
-}
+}*/

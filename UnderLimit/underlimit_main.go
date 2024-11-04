4a0469d9fcca285c4ee033dbd18bbf2884e6643e
neandrson <a2361@yandex.ru>
2024-11-04 17:01:47 +0300

    Update underlimit_main.go

--- UnderLimit/underlimit_main.go
+++ UnderLimit/underlimit_main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"testing"
 )
 
 func UnderLimit(nums []int, limit int, n int) ([]int, error) {
@@ -22,97 +22,31 @@ func UnderLimit(nums []int, limit int, n int) ([]int, error) {
 		}
 	}
 	return result, nil*/
+	// Проверка на отрицательное значение n
 	if n < 0 {
-		return nil, fmt.Errorf("n cannot be negative")
+		return nil, errors.New("n cannot be negative")
 	}
-	if nums == nil {
-		return nil, fmt.Errorf("nums cannot be nil")
-	}
-	if n == 0 {
-		return []int{}, nil
-	}
-
-	result := make([]int, 0, n)
-	count := 0
+	// Инициализация результирующего слайса
+	result := []int{}
+	// Перебор элементов в слайсе
 	for _, num := range nums {
-		if count == n {
-			return result, nil
-		}
 		if num < limit {
 			result = append(result, num)
-			count++
+			if len(result) == n {
+				break
+			}
 		}
 	}
+
 	return result, nil
 }
 
-func TestUnderLimit(t *testing.T) {
-	type test struct {
-		nums      []int
-		n         int
-		limit     int
-		expected  []int
-		wantError bool
-	}
-
-	tests := []test{
-		{
-			nums:      []int{4, 7, 89, 3, 21, 2, 5, 7, 32, 4, 6, 8, 0, 3, 4, 6, 2, 115, 12},
-			n:         5,
-			limit:     3,
-			expected:  []int{2, 0, 2},
-			wantError: false,
-		},
-		{
-			nums:      nil,
-			wantError: true,
-		},
-		{
-			nums:      []int{},
-			n:         5,
-			limit:     3,
-			expected:  []int{},
-			wantError: false,
-		},
-		{
-			nums:      []int{3, 5, 6},
-			n:         5,
-			limit:     10,
-			expected:  []int{3, 5, 6},
-			wantError: false,
-		},
-		{
-			nums:      []int{-13, 0, 6},
-			n:         1,
-			limit:     -5,
-			expected:  []int{-13},
-			wantError: false,
-		},
-		{
-			nums:      []int{},
-			n:         -1,
-			limit:     5,
-			wantError: true,
-		},
-	}
-
-	for _, tc := range tests {
-		result, err := UnderLimit(tc.nums, tc.limit, tc.n)
-		if tc.wantError {
-			if err == nil {
-				t.Fatalf("Expected", tc.n, "but got %d", result)
-			}
-
-		}
-	}
-}
-
 func main() {
-	/*nums := []int{5, 3, 10, 7, 2, 8}
+	nums := []int{5, 3, 10, 7, 2, 8}
 	limit := 6
-	n := 3*/
+	n := 3
 
-	result, err := TestUnderLimit()
+	result, err := TestUnderLimit(nums[], limit, n)
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {

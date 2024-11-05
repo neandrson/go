fa4551116b63e2637611fe3fee23ef9a4738ffdb
neandrson <a2361@yandex.ru>
2024-11-05 22:23:51 +0300

    Update concatenatestrings_main.go

--- 0/ConcatenateStrings/concatenatestrings_main.go
+++ 0/ConcatenateStrings/concatenatestrings_main.go
@@ -1,9 +1,15 @@
 package main
 
-func ConcatenateStrings(str1, str2 string) string {
+import (
+	"fmt"
+	"strings"
+)
 
+func ConcatenateStrings(str1, str2 string) string {
+	words := []string{str1, str2}
+	return strings.Join(words, " ")
 }
 
 func main() {
-
+	fmt.Println(ConcatenateStrings("Ira", "Khomyakova"))
 }

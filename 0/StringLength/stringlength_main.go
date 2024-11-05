1f77f387a3e8e352ece59cfa5623c658bf9b50ce
neandrson <a2361@yandex.ru>
2024-11-05 22:17:23 +0300

    Update stringlength_main.go

--- 0/StringLength/stringlength_main.go
+++ 0/StringLength/stringlength_main.go
@@ -1,9 +1,11 @@
 package main
 
-func StringLength(input string) int {
+import "fmt"
 
+func StringLength(input string) int {
+	return len(input)
 }
 
 func main() {
-
+	fmt.Println(StringLength("Tyatya"))
 }

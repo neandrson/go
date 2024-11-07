87bbe1dcdc24f34a2883840ac432e15c9d061296
neandrson <a2361@yandex.ru>
2024-11-07 14:21:27 +0300

    Update findmaxkey_main.go

--- 0/FindMaxKey/findmaxkey_main.go
+++ 0/FindMaxKey/findmaxkey_main.go
@@ -1,9 +1,25 @@
 package main
 
-func FindMaxKey(m map[int]int) int {
+import "fmt"
 
+func FindMaxKey(m map[int]int) int {
+	var maxNumber int
+	for n := range m {
+		maxNumber = n
+		break
+	}
+	for n := range m {
+		if n > maxNumber {
+			maxNumber = n
+		}
+	}
+	return maxNumber
 }
 
 func main() {
-
+	m := map[int]int{
+		-7: 1,
+		-1: 38,
+	}
+	fmt.Println(FindMaxKey(m))
 }

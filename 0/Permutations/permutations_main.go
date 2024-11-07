1595f68261765262500d9ad29495d767874ca244
neandrson <a2361@yandex.ru>
2024-11-07 14:21:32 +0300

    Update permutations_main.go

--- 0/Permutations/permutations_main.go
+++ 0/Permutations/permutations_main.go
@@ -2,25 +2,40 @@ package main
 
 import "fmt"
 
+var subPermutations []string
+
 func merge(ins []rune, c rune) (result []string) {
-	for i := 0; i <= len(ins); i++ {
+	for i := len(ins); i >= 0; i-- {
+		//fmt.Println(result)
+		//fmt.Println(i)
+		//fmt.Println(ins[:i])
+		//fmt.Println(c)
+		fmt.Println(ins[i:])
 		result = append(result, string(ins[:i])+string(c)+string(ins[i:]))
+		//fmt.Println(result)
 	}
 	return
 }
 
 func Permutations(input string) []string {
-	if len(input) == 1 {
+
+	if len(input) <= 1 {
 		return []string{input}
 	}
-
+	//fmt.Println(input)
 	runes := []rune(input)
-	subPermutations := Permutations(string(runes[0 : len(input)-1]))
+	//fmt.Println(runes)
+
+	for i := 0; i < len(input)-1; i++ {
+		subPermutations = Permutations(string(runes[i : len(input)-1]))
+		//fmt.Println(subPermutations)
+	}
 
 	result := []string{}
 	for _, s := range subPermutations {
+		//fmt.Println(merge([]rune(s), runes[len(input)-1]))
 		result = append(result, merge([]rune(s), runes[len(input)-1])...)
-		fmt.Println(result)
+		//fmt.Println(result)
 	}
 
 	return result
@@ -44,7 +59,56 @@ func PermutationsTailRecursive(input string) []string {
 	return helper(runes[1:], []string{string(runes[0])})
 }
 
+/*func Perm(input string) {
+	a := []rune(input)
+	f (a)
+	perm(a, f, 0)
+}
+
+// Permute the values at index i to len(a)-1.
+func perm(a []rune, f func([]rune), i int) {
+	if i > len(a) {
+		f(a)
+		return
+	}
+	perm(a, f, i+1)
+	for j := i + 1; j < len(a); j++ {
+		a[i], a[j] = a[j], a[i]
+		perm(a, f, i+1)
+		a[i], a[j] = a[j], a[i]
+	}
+}*/
+
+/*func join(ins []rune, c rune) (result []string) {
+	for i := len(ins); i >= 0; i-- {
+		result = append(result, string(ins[:i])+string(c)+string(ins[i:])) //
+	}
+	return
+}
+
+func Permutations(testStr string) []string {
+	var n func(testStr []rune, p []string) []string
+	n = func(testStr []rune, p []string) []string {
+		if len(testStr) == 0 {
+			return p
+		} else {
+			result := []string{}
+			for _, e := range p {
+				result = append(result, join([]rune(e), testStr[0])...)
+			}
+			return n(testStr[1:], result)
+		}
+	}
+
+	output := []rune(testStr)
+	return n(output[1:], []string{string(output[0])})
+}*/
+
 func main() {
 	arr := "abc"
 	fmt.Println(Permutations(arr))
+	/*Perm([]rune("abc"), func(a []rune) {
+		fmt.Println(string(a))
+	})*/
+
 }

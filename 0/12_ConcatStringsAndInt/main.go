78ac0f5626acc557a3e0138783a1307c8b574c97
neandrson <a2361@yandex.ru>
2024-11-10 20:04:19 +0300

    Update main.go

--- 0/12_ConcatStringsAndInt/main.go
+++ 0/12_ConcatStringsAndInt/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func ConcatStringsAndInt(str1, str2 string, num int) string {
-	return str1 + " " + str2 + " " + string(num)
+	return str1 + str2 + strconv.Itoa(num)
 }
 
 func main() {

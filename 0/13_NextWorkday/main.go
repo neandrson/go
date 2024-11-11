5f41e1d55cdec0a593247626e7dc959bfd2feb69
neandrson <a2361@yandex.ru>
2024-11-11 14:31:08 +0300

    Create main.go

---
+++ 0/13_NextWorkday/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func NextWorkday(start time.Time) time.Time {
+	nextDay := startTime.AddDate(0, 0, 1)
+
+	for nextDay.Weekday() == time.Saturday || nextDay.Weekday() == time.Sunday {
+		nextDay = nextDay.AddDate(0, 0, 1)
+	}
+
+	return nextDay
+}
+
+func main() {
+	startTime := time.Date(2022, time.March, 25, 0, 0, 0, 0, time.UTC)
+	nextWorkday := NextWorkday(startTime)
+
+	fmt.Println(nextWorkday)
+}

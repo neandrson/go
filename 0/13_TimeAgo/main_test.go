01f7b9665dd2e759bb3ae01b27b7c2cef75de74a
neandrson <a2361@yandex.ru>
2024-11-11 15:20:39 +0300

    Update main_test.go

--- 0/13_TimeAgo/main_test.go
+++ 0/13_TimeAgo/main_test.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
-
-func TimeAgo(pastTime time.Time) string {
-	duration := time.Since(pastTime)
-
-	if duration.Seconds() < 60 {
-		return fmt.Sprintf("%.0f secundes ago", duration.Seconds())
-	} else if duration.Minutes() < 60 {
-		return fmt.Sprintf("%.0f minutes ago", duration.Minutes())
-	} else if duration.Hours() < 24 {
-		return fmt.Sprintf("%.0f hours ago", duration.Hours())
-	} else if duration.Hours() < 24*7 {
-		return fmt.Sprintf("%.0f дней назад", duration.Hours()/24)
-	} else if duration.Hours() < 24*30 {
-		return fmt.Sprintf("%.0f недель назад", duration.Hours()/(24*7))
-	} else if duration.Hours() < 24*365 {
-		return fmt.Sprintf("%.0f month ago", duration.Hours()/(24*30))
-	} else {
-		return fmt.Sprintf("%.0f yar ago", duration.Hours()/(24*365))
-	}
-}
 
 func TestTimeAgo(t *testing.T) {
 	pastTime := time.Now().Add(-2 * time.Hour)

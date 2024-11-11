99345f09fa745629f0d839242f5731790e38de9c
neandrson <a2361@yandex.ru>
2024-11-11 20:00:05 +0300

    Create main_test.go

---
+++ 0/14_DeleteVowels/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestDeleteVowels(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"hello", "hll"},
+		{"world", "wrld"},
+		{"apple", "ppl"},
+		//{"banana", "bnn"},
+		//{"", ""},
+		{"elephant", "lphnt"},
+		{"orange", "rng"},
+	}
+
+	for _, test := range tests {
+		result := DeleteVowels(test.input)
+		if result != test.expected {
+			t.Errorf("For input %s, expected %s, but got %s", test.input, test.expected, result)
+		}
+	}
+}

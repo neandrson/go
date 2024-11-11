f948e83846baaad44bc3b82b0c7de7cc04a351e1
neandrson <a2361@yandex.ru>
2024-11-11 12:06:11 +0300

    Update main.go

--- 0/12_Factorial/main.go
+++ 0/12_Factorial/main.go
@@ -9,7 +9,11 @@ func Factorial(n int) (int, error) {
 	if n < 0 {
 		return 0, errors.New("factorial is not defined for negative numbers")
 	}
-	return n * Factorial(n-1), nil
+	factorial := 1
+	for i := 2; i <= n; i++ {
+		factorial *= i
+	}
+	return factorial, nil
 }
 
 func main() {

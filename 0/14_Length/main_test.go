90df80a4f52b2bbd50c9f3267d41677b0b8408e9
neandrson <a2361@yandex.ru>
2024-11-11 15:30:45 +0300

    Create main_test.go

---
+++ 0/14_Length/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+type Test struct {
+	in  int
+	out string
+}
+
+var tests = []Test{
+	{-1, "negative"},
+	{5, "short"},
+}
+
+func TestLength(t *testing.T) {
+	for i, test := range tests {
+		size := Length(test.in)
+		if size != test.out {
+			t.Errorf("#%d: Size(%d)=%s; want %s", i, test.in, size, test.out)
+		}
+	}
+}

cf0898d6da5cf70eaf7ca24b3bc6b16647340433
neandrson <a2361@yandex.ru>
2024-11-13 08:05:24 +0300

    Update main.go

--- 1/3_Run/main.go
+++ 1/3_Run/main.go
@@ -9,11 +9,11 @@ func run() error {
 	if len(os.Args) < 3 {
 		return fmt.Errorf("необходимо указать размер сетки и процент заполнения")
 	}
-	a := string.os.Args[1]
+	a := os.Args[1]
 	b := os.Args[2]
 	c := os.Args[3]
-	if a < "0" || b < "0" || c < "0" {
-		return fmt.Errorf("Не должно быть нулем")
+	if a < "0" || b < "0" || c <= "0" {
+		return fmt.Errorf("Должно быть больше нуля")
 	}
 
 	gridSize := os.Args[1] + "x" + os.Args[2]

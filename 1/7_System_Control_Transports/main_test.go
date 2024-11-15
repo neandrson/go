cd6e1b0a0fc14e0fa71576b014e2268105d18c21
neandrson <a2361@yandex.ru>
2024-11-15 19:25:51 +0300

    Create main_test.go

---
+++ 1/7_System_Control_Transports/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestEstimateTravelTime(t *testing.T) {
+	car := Car{Type: "Седан", Speed: 60.0, FuelType: "Бензин"}
+	motorcycle := Motorcycle{Type: "Мотоцикл", Speed: 80.0}
+
+	vehicles := []Vehicle{car, motorcycle}
+
+	distance := 200.0
+
+	travelTimes := EstimateTravelTime(vehicles, distance)
+	fmt.Println(travelTimes)
+	expectedCarTime := distance / car.Speed
+	if travelTimes["main.Car"] != expectedCarTime {
+		t.Errorf("Ожидается время для автомобиля %.2f часа, получено %.2f", expectedCarTime, travelTimes["main.Car"])
+	}
+
+	expectedMotorcycleTime := distance / motorcycle.Speed
+	if travelTimes["main.Motorcycle"] != expectedMotorcycleTime {
+		t.Errorf("Ожидается время для мотоцикла %.2f часа, получено %.2f", expectedMotorcycleTime, travelTimes["main.Motorcycle"])
+	}
+}

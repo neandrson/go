0bea9fb296ead027adb1d6c1343716642ff8ff39
neandrson <a2361@yandex.ru>
2024-11-15 21:35:56 +0300

    Create main_test.go

---
+++ 1/8_Reports/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateReport(t *testing.T) {
+	user := User{ID: 1, Name: "Иван", Email: "ivan@example.com", Age: 30}
+	reportDate := time.Now().Format("2006-01-02")
+
+	report := CreateReport(user, reportDate)
+	fmt.Println(report)
+	// Проверяем, что поля в отчете заполнены корректно
+	if report.ID != user.ID {
+		t.Errorf("Ожидается ID пользователя %d, получено %d", user.ID, report.ID)
+	}
+	if report.Name != user.Name {
+		t.Errorf("Ожидается имя пользователя %s, получено %s", user.Name, report.Name)
+	}
+	if report.Email != user.Email {
+		t.Errorf("Ожидается Email пользователя %s, получено %s", user.Email, report.Email)
+	}
+	if report.Age != user.Age {
+		t.Errorf("Ожидается возраст пользователя %d, получено %d", user.Age, report.Age)
+	}
+	if report.ReportID <= 0 {
+		t.Errorf("Ожидается положительное значение, получено %d", report.ReportID)
+	}
+	t, err := time.Parse("02-01-2006", reportDate)
+	if err != nil {
+		t.Errorf("Неверная дата", err)
+	}
+}

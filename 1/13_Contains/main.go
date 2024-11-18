a9219c21165f97d692a85ca330942d400da9e17d
neandrson <a2361@yandex.ru>
2024-11-18 16:36:41 +0300

    Update main.go

--- 1/13_Contains/main.go
+++ 1/13_Contains/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
+	"fmt"
 	"io"
+	"strings"
 )
 
 func Contains(r io.Reader, seq []byte) (bool, error) {
-	br := bufio.NewReader(r)
+	/*br := bufio.NewReader(r)
 	buf := make([]byte, len(seq))
 	for {
 		n, err := br.Read(buf)
@@ -23,9 +23,43 @@ func Contains(r io.Reader, seq []byte) (bool, error) {
 			}
 		}
 	}
+	return false, nil*/
+
+	buf := make([]byte, len(seq))
+	pos := 0
+
+	for {
+		n, err := r.Read(buf)
+		if err != nil && err != io.EOF {
+			return false, err
+		}
+		if n == 0 {
+			break
+		}
+
+		for i := 0; i < n; i++ {
+			if buf[i] == seq[pos] {
+				pos++
+				if pos == len(seq) {
+					return true, nil
+				}
+			} else {
+				pos = 0
+			}
+		}
+	}
+
 	return false, nil
 }
 
 func main() {
+	r := strings.NewReader("hello world")
+	seq := []byte("world")
 
+	found, err := Contains(r, seq)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Sequence found:", found)
+	}
 }

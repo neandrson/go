7f3fae52a10ea9b986e447a22f6f4b712af64e38
neandrson <a2361@yandex.ru>
2024-11-18 21:33:52 +0300

    Update main.go

--- 1/16_CopyFilePart/main.go
+++ 1/16_CopyFilePart/main.go
@@ -1,7 +1,34 @@
 package main
 
-func CopyFilePart(inputFilename, outFileName string, startpos int) error {
+import (
+	"io"
+	"os"
+)
 
+func CopyFilePart(inputFilename, outFileName string, startPos int) error {
+
+	inputFile, err := os.Open(inputFilename)
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	_, err = inputFile.Seek(int64(startPos), 0)
+	if err != nil {
+		return err
+	}
+
+	outputFile, err := os.Create(outFileName)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {

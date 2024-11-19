262040e24bc5165cef199211decd3a8d105307e2
neandrson <a2361@yandex.ru>
2024-11-19 08:42:43 +0300

    Create main.go

---
+++ 1/18_ExtractLog/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func ExtractLog(inputFileName string, start, end time.Time) ([]string, error) {
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var logs []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		timestampStr := line[:10] // Предполагаем, что дата всегда занимает первые 10 символов строки
+		timestamp, err := time.Parse("02.01.2006", timestampStr)
+		if err != nil {
+			return nil, err
+		}
+
+		if timestamp.After(start) && timestamp.Before(end) {
+			logs = append(logs, line)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return logs, nil
+}
+
+func main() {
+	start := time.Date(2022, time.January, 1, 0, 0, 0, 0, time.UTC)
+	end := time.Date(2022, time.January, 31, 23, 59, 59, 999, time.UTC)
+
+	logs, err := ExtractLog("logfile.txt", start, end)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, log := range logs {
+		fmt.Println(log)
+	}
+}

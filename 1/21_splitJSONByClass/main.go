44f36e826950d15af3ced96affa2a9572e825170
neandrson <a2361@yandex.ru>
2024-11-20 13:38:38 +0300

    Update main.go

--- 1/21_splitJSONByClass/main.go
+++ 1/21_splitJSONByClass/main.go
@@ -5,39 +5,51 @@ import (
 	"fmt"
 )
 
+type Student struct {
+	Name  string `json:"name"`
+	Class string `json:"class"`
+}
+
 func splitJSONByClass(jsonData []byte) (map[string][]byte, error) {
-	var data []map[string]interface{}
-	err := json.Unmarshal(jsonData, &data)
+	var students []Student
+	err := json.Unmarshal(jsonData, &students)
 	if err != nil {
 		return nil, err
 	}
 
+	classMap := make(map[string][]Student)
 	result := make(map[string][]byte)
 
-	for _, item := range data {
-		class := item["class"].(string)
-		jsonBytes, err := json.Marshal(item)
+	for _, student := range students {
+		fmt.Println(student)
+		classMap[student.Class] = append(classMap[student.Class], student)
+		fmt.Println(classMap[student.Class])
+	}
+
+	for class, students := range classMap {
+		jsonStudents, err := json.Marshal(students)
 		if err != nil {
 			return nil, err
 		}
-		result[class] = append(result[class], jsonBytes)
+		result[class] = jsonStudents
 	}
 
-return result, nil
+	return result, nil
 }
 
-func main() { jsonData := []byte([ {"class": "A", "name": "Alice"}, {"class": "B", "name": "Bob"}, {"class": "A", "name": "Alex"} ])
+func main() {
+	jsonData := []byte(`[{"class": "A", "name": "Alice"}, {"class": "B", "name": "Bob"}, {"class": "A", "name": "Alex"}]`)
 
-result, err := splitJSONByClass(jsonData)
-if err != nil {
-	fmt.Println("Error:", err)
-	return
-}
-
-for class, jsonList := range result {
-	fmt.Printf("Class: %s\n", class)
-	for _, jsonData := range jsonList {
-		fmt.Println(string(jsonData))
+	result, err := splitJSONByClass(jsonData)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
 	}
-}
+
+	for class, jsonList := range result {
+		fmt.Printf("Class: %s", class)
+		for _, jsonData := range jsonList {
+			fmt.Print(string(jsonData))
+		}
+	}
 }

2dbde1103bcd7248ccb04a39aa88e1bb5eab19a9
neandrson <a2361@yandex.ru>
2024-11-20 22:00:15 +0300

    Create main.go

---
+++ 1/1.5.8_UnmarshalJSON/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "encoding/json"
+
+type NullString struct {
+	String string
+	Valid  bool
+}
+
+func (ns *NullString) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		ns.Valid = false
+		return nil
+	}
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+	ns.String = str
+	ns.Valid = true
+	return nil
+}

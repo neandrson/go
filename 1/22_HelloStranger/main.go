b459ae4019d328eb724b03c6fcf06376a75ab059
neandrson <a2361@yandex.ru>
2024-11-21 15:18:40 +0300

    Create main.go

---
+++ 1/22_HelloStranger/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	if name == "" {
+		fmt.Fprintf(w, "hello stranger")
+		return
+	}
+
+	match, _ := regexp.MatchString("^[a-zA-Z]*$", name)
+	if !match {
+		fmt.Fprintf(w, "hello dirty hacker")
+		return
+	}
+
+	fmt.Fprintf(w, "hello %s", name)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}

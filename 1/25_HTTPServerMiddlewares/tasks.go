3abb9886b438678a5505da4686a424ef1a55fe01
neandrson <a2361@yandex.ru>
2024-11-22 22:21:29 +0300

    Create tasks.go

---
+++ 1/25_HTTPServerMiddlewares/tasks.go
@@ -0,0 +1,44 @@
+package tasks
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func SetDefaultName(h http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if len(name) == 0 {
+			r.URL.Query().Set("name", "stranger")
+		}
+	})
+}
+
+func Sanitize(h http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		var dirty bool = false
+		for _, n := range "0123456789_[]{}./,()`!@#$%^&*-+=\"'" {
+			if strings.ContainsRune(name, n) {
+				dirty = true
+			}
+		}
+		if dirty {
+			r.URL.Query().Set("name", "dirty hacker")
+		}
+	})
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	fmt.Fprint(w, "hello ", name)
+}
+
+func tasks() {
+	handler := SetDefaultName(Sanitize(HelloHandler))
+
+	http.Handle("/", handler)
+
+	http.ListenAndServe(":8080", nil)
+}

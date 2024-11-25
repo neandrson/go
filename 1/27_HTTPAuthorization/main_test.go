1d822b30a6daa601bc00c6658b6e799a6ecd967e
neandrson <a2361@yandex.ru>
2024-11-25 15:16:13 +0300

    Create main_test.go

---
+++ 1/27_HTTPAuthorization/main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAuthorizationMiddleware(t *testing.T) {
+	req, err := http.NewRequest("GET", "/answer/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Добавляем заголовок Authorization с корректными данными
+	req.Header.Set("Authorization", "Basic userid:password")
+
+	rr := httptest.NewRecorder()
+
+	handler := Authorization(answerHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	expected := "The answer is 42"
+	if rr.Body.String() != expected {
+		t.Errorf("Expected response body %s, got %s", expected, rr.Body.String())
+	}
+}
+
+func TestAuthorizationMiddlewareUnauthorized(t *testing.T) {
+	req, err := http.NewRequest("GET", "/answer/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Не добавляем заголовок Authorization
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(Authorization(answerHandler))
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, rr.Code)
+	}
+
+	expected := "The answer is 42"
+	if rr.Body.String() != expected {
+		t.Errorf("Expected response body %s, got %s", expected, rr.Body.String())
+	}
+}

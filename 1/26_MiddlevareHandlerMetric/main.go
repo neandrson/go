3479d2b91f49835f5d96ef307c7485f1e0e34923
neandrson <a2361@yandex.ru>
2024-11-25 13:07:20 +0300

    Update main.go

--- 1/26_MiddlevareHandlerMetric/main.go
+++ 1/26_MiddlevareHandlerMetric/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 var requestCount int
 
-func Metrics(next http.HandlerFunc) http.HandlerFunc {
+func Metrics(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
+		//start := time.Now()
 		next.ServeHTTP(w, r)
-		duration := time.Since(start)
-		fmt.Printf("rpc_duration_milliseconds_count %d\n", duration)
-		requestCount++
+		//duration := time.Since(start)
+		fmt.Printf("rpc_duration_milliseconds_count %d\n", requestCount)
+		//requestCount++
 	})
 }
 
@@ -28,16 +27,20 @@ func fibonacci(n int) int {
 func FibonacciHandler(w http.ResponseWriter, r *http.Request) {
 	n := fibonacci(requestCount)
 	fmt.Fprintf(w, "%d", n)
+	requestCount++
 }
 
 func MetricsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "rpc_duration_milliseconds_count %d", requestCount)
+	//RequestCount++
 }
 
 func main() {
-	fibonacciHandler := http.HandlerFunc(fibonacciHandler)
-	metricsHandler := http.HandlerFunc(metricsHandler)
+	fibonacciHandler := http.HandlerFunc(FibonacciHandler)
+	metricsHandler := http.HandlerFunc(MetricsHandler)
+
 	http.Handle("/", Metrics(fibonacciHandler))
 	http.Handle("/metrics", metricsHandler)
+
 	http.ListenAndServe(":8080", nil)
 }

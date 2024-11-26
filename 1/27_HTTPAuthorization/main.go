ab90df324c2851169588440f23ca69cb7be645ed
neandrson <a2361@yandex.ru>
2024-11-26 13:14:42 +0300

    Update main.go

--- 1/27_HTTPAuthorization/main.go
+++ 1/27_HTTPAuthorization/main.go
@@ -23,40 +23,76 @@ import (
 */
 
 func Authorization(next http.HandlerFunc) http.HandlerFunc {
-	username := []byte("admin")
-	password := []byte("123456")
-
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			http.Error(w, "Authorization required", http.StatusForbidden)
+			//http.Error(w, "Unauthorized", http.StatusForbidden)
+			//w.Write([]byte("Unauthorized"))
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
 		auth := strings.SplitN(authHeader, " ", 2)
+		//fmt.Println(auth)
 		if len(auth) != 2 || auth[0] != "Basic" {
-			http.Error(w, "Invalid authorization", http.StatusUnauthorized)
+			//http.Error(w, "Unauthorized ", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusBadRequest)
+			//w.Write([]byte("Unauthorized"))
 			return
 		}
 
 		data, err := base64.StdEncoding.DecodeString(auth[1])
 		if err != nil {
-			http.Error(w, "The answer is 42", http.StatusUnauthorized)
+			//http.Error(w, "Unauthorized ", http.StatusUnauthorized)
+			w.WriteHeader(http.StatusBadRequest)
+			//w.Write([]byte("Unauthorized"))
 			return
 		}
-
-		if string(data) != "username:password" {
+		//fmt.Println(string(data))
+		/*if string(data) != "John:123456" {
 			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
 			return
-		}
+		}*/
 
+		usernamePassword := strings.SplitN(string(data), ":", 2)
+		if len(usernamePassword) != 2 || !checkCredentials(usernamePassword[0], usernamePassword[1]) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		/*u, p, ok := r.BasicAuth()
+		fmt.Println(u, p)
+		if !ok {
+			//fmt.Println("Error parsing basic auth")
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorized "))
+			return
+		}*/
+		
+		//fmt.Printf("Username: %s\n", u)
+		//fmt.Printf("Password: %s\n", p)
+
+		w.WriteHeader(200)
 		next.ServeHTTP(w, r)
+		w.Write([]byte("The answer is 42"))
+		return
 	}
+}
+
+func checkCredentials(u, p string) bool {
+	username := "John"
+	password := "123456"
+	// Здесь можно добавить логику проверки реальных учетных данных
+	if username != u || password != p {
+		//fmt.Printf("Username provided is correct: %s\n", u)
+		return true
+	}
+	return falsh
 }
 
 func answerHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "The answer is 42")
-	w.Write([]byte("The answer is 42\n"))
+	w.Write([]byte("The answer is 42"))
 }
 
 func main() {
@@ -65,13 +101,3 @@ func main() {
 		panic(err)
 	}
 }
-
-/*func main() {
-	//username := []byte("admin")
-	//password := []byte("123456")
-
-	http.HandleFunc("/answer/", Authorization(answerHandler))
-
-	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}*/

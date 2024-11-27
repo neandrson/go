2fe816daea51de9b374bdac9789b6bea924871f6
neandrson <a2361@yandex.ru>
2024-11-27 16:58:43 +0300

    Update main.go

--- 1/28_JSONHelloStranger/main.go
+++ 1/28_JSONHelloStranger/main.go
@@ -16,63 +16,6 @@ type Response struct {
 	Result Result `json:"result"`
 }
 
-/*func (wr *wrappedResponseWriter) WriteHeader(code int) {
-	wr.statusCode = code
-	wr.ResponseWriter.WriteHeader(code)
-}
-
-func Sanitize(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := r.Context().Value("name").(string)
-
-		if !regexp.MustCompile(`^[a-zA-Z]+$`).MatchString(name) && name != "" {
-			panic("invalid name format")
-		}
-		next(w, r)
-	}
-}
-
-func SetDefaultName(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Query().Get("name")
-		if name == "" {
-			name = "stranger"
-		}
-		ctx := context.WithValue(r.Context(), "name", name)
-		next(w, r.WithContext(ctx))
-	}
-}
-
-func RPC(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-
-				response := map[string]interface{}{
-					"status": "error",
-					"result": map[string]interface{}{},
-				}
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusOK)
-				json.NewEncoder(w).Encode(response)
-				return
-			}
-		}()
-
-		name := r.Context().Value("name").(string)
-
-		response := map[string]interface{}{
-			"status": "ok",
-			"result": map[string]interface{}{
-				"greetings": "hello",
-				"name":      name,
-			},
-		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	}
-}*/
-
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
 	response := Response{Status: "ok", Result: Result{Greetings: "hello", Name: name}}
@@ -139,13 +82,14 @@ func Sanitize(next http.HandlerFunc) http.HandlerFunc {
 				// Возвращаем ответ с ошибкой
 				//w.Header().Set("Content-Type", "application/json")
 				//w.WriteHeader(200)
-				output := Response{Status: "error", Result: Result{}}
-				jsonOutput, err := json.Marshal(output)
+				//output := Response{Status: "error", Result: Result{}}
+				/*jsonOutput, err := json.Marshal(output)
 				if err != nil {
 					return
-				}
+				}*/
+				jsonOutput := `{"status":"error","result":{}}`
 				//json.NewEncoder(w).Encode(jsonOutput)
-				w.Write(jsonOutput)
+				w.Write([]byte(jsonOutput))
 				//next.ServeHTTP(w, r)
 				return
 			}

ec9add8887836416915d41d9803f6e8c4f88407b
neandrson <a2361@yandex.ru>
2024-11-28 14:12:56 +0300

    Update main.go

--- 1/1.10.4_Zap/main.go
+++ 1/1.10.4_Zap/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"go.uber.org/zap"
 )
 
 func main() {
-	logger, _ := zap.NewProduction()
+	/*logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	logger.Info("Hello, World!")
+	logger.Info("Hello, World!")*/
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
+
+	logger.Info("Hello, world!")
 }

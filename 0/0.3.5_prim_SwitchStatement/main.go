cda4ff884e5645f0935f4692d60e8e6bc6673d72
neandrson <a2361@yandex.ru>
2024-11-30 19:48:31 +0300

    Update main.go

--- 0/0.3.5_prim_SwitchStatement/main.go
+++ 0/0.3.5_prim_SwitchStatement/main.go
@@ -33,3 +33,34 @@ func main() {
 		fmt.Println("i равно нулю")
 	}
 }
+
+/*package main
+
+import "fmt"
+
+func main() {
+    i := 2
+    if i == 1 {
+        fmt.Println("i равно 1")
+    } else if i == 2 {
+        fmt.Println("i равно 2")
+    } else {
+        fmt.Println("i не равно ни 1, ни 2")
+    }
+}*/
+
+/*package main
+
+import "fmt"
+
+func main() {
+    i := 2
+    switch i {
+    case 1:
+        fmt.Println("i равно 1")
+    case 2:
+        fmt.Println("i равно 2")
+    default:
+        fmt.Println("i не равно ни 1, ни 2")
+    }
+}*/

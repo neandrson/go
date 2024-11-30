df0f49332a3632eb72e73e0b87d97bca31187bc5
neandrson <a2361@yandex.ru>
2024-11-30 19:36:50 +0300

    Update main.go

--- 0/0.2.5_ptim_FirstProgram/main.go
+++ 0/0.2.5_ptim_FirstProgram/main.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var age int
+	/*var age int
 	fmt.Println("Введите ваш возраст:")
 	fmt.Scanln(&age)
+	fmt.Println(age)*/
+	var age int
+	fmt.Println("Введите ваш возраст:")
+	_, err := fmt.Scanln(&age)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(age)
 }
+
+/*package main // создаём пакет main для нашей программы
+
+import "fmt" // подключаем библиотеку fmt для работы со строками и консолью
+
+func main() { // обозначаем точку входа в нашу программу
+	var age int // определяем переменную для хранения возраста
+	fmt.Println("Введите ваш возраст:") // выводим сообщение с просьбой ввести возраст
+	fmt.Scanln(&age)  // просим пользователя ввести число в консоль
+	fmt.Println(age)  // выводим в консоль его же возраст
+}*/

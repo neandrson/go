b808e69e35af99b6300546c86fc6b2d862cb0320
neandrson <a2361@yandex.ru>
2024-12-02 09:07:44 +0300

    Create main2.go

---
+++ 0/0.14.3_prim_TestCountMap/main2.go
@@ -0,0 +1,23 @@
+package lenght
+
+import (
+	"fmt"
+)
+
+func Length(a int) string {
+	switch {
+	case a < 0:
+		return "negative"
+	case a == 0:
+		return "zero"
+	case a < 10:
+		return "short"
+	case a < 100:
+		return "long"
+	}
+	return "very long"
+}
+
+func main() {
+	fmt.Println(Length(90))
+}

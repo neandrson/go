f272ea88e27fffe7a02860e7e4a678475d8d9196
neandrson <a2361@yandex.ru>
2024-12-17 11:34:04 +0300

    Create main.go

---
+++ 1/1.7.3_HandlerMetric/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var requestCount int
+
+func Metrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//start := time.Now()
+		next.ServeHTTP(w, r)
+		//duration := time.Since(start)
+		fmt.Printf("rpc_duration_milliseconds_count %d\n", requestCount)
+		//requestCount++
+	})
+}
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func FibonacciHandler(w http.ResponseWriter, r *http.Request) {
+	n := fibonacci(requestCount)
+	fmt.Fprintf(w, "%d", n)
+	requestCount++
+}
+
+func MetricsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "rpc_duration_milliseconds_count %d", requestCount)
+	//RequestCount++
+}
+
+func main() {
+	fibonacciHandler := http.HandlerFunc(FibonacciHandler)
+	metricsHandler := http.HandlerFunc(MetricsHandler)
+
+	http.Handle("/", Metrics(fibonacciHandler))
+	http.Handle("/metrics", metricsHandler)
+
+	http.ListenAndServe(":8080", nil)
+}

33b98661a462a4a84d8818f51a1049d4820b06ff
neandrson <a2361@yandex.ru>
2024-12-17 10:21:02 +0300

    Create main.go

---
+++ 1/1.3.1_prim_ioReader/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"io"
+	"os"
+)
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+/*func ProcessData(ctx context.Context, bucket, fileName string) error{
+	// Клиент для работы с хранилищем
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		// TODO: handle error.
+	}
+	// Откроем файл в Google Cloud Storage
+	file, err := client.Bucket(bucket).Object(filename).NewReader(ctx)
+	if err != nil {
+		// TODO: handle error.
+	}
+	// Здесь обрабатываем данные
+	...
+}*/
+
+func OpenLocalFile(filename string) (*os.File, error) {
+	// Открываем локальный файл
+}
+func OpenGCSFile(
+	ctx context.Context,
+	bucket, fileName string,
+) (*storage.Reader, error) {
+	// Открываем файл из Google Cloud Storage
+}
+
+func ProcessData(ctx context.Context, reader io.Reader) error {
+	// Здесь обрабатываем данные
+	//...
+}

6cb195fb52f07a32183a27772565f7c2b62ff633
neandrson <a2361@yandex.ru>
2024-12-17 11:29:54 +0300

    Create main.go

---
+++ 1/1.10.1_WriteToLogFile/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func WriteToLogFile(message string, fileName string) error {
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+	// Закрываем файл после выхода из main
+	defer file.Close()
+	// Конфигурируем логгер, чтобы он выводил лог в файл
+	log.SetOutput(file)
+
+	log.Println(message)
+	return nil
+}
+
+func main() {
+	mes := "hello world"
+	fileName := "output.txt"
+
+	fmt.Println(WriteToLogFile(mes, fileName))
+}

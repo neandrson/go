20d6f477800f42c162061cf728c9d8f6a927a655
neandrson <a2361@yandex.ru>
2024-12-17 11:24:09 +0300

    Update main.go

--- 1/1.3.1_prim_ioWrite/main.go
+++ 1/1.3.1_prim_ioWrite/main.go
@@ -10,12 +10,14 @@ type Writer interface {
 }
 
 func WriteData(writer io.Writer, data []byte) error {
+	// Пример 1
 	/*bytesWritten, err := writer.Write(data) // Записываем данные
 	  if err != nil {
 	     // TODO: handle error.
 	  }
 	  fmt.Printf("Записано %d байт", bytesWritten)*/
 
+	// Пример 2
 	file, err := os.Create("output.txt") // Создаём файл
 	if err != nil {
 		// TODO: handle error.
@@ -26,3 +28,25 @@ func WriteData(writer io.Writer, data []byte) error {
 		// TODO: handle error.
 	}
 }
+
+// Пример 2
+type myWriter struct {
+	content []byte // Сюда будем записывать данные
+}
+
+// Реализуем интерфейс io.Writer
+func (w *myWriter) Write(buf []byte) (int, error) {
+	w.content = append(w.content, buf...) // Запишем данные в слайс
+	return len(buf), nil                  // Вернём, сколько данных записано
+}
+
+// Для удобного представления реализуем интерфейс Stringer
+func (w *myWriter) String() string {
+	return string(w.content)
+}
+
+/*func WriteData(writer io.Writer, data []byte) error {
+	w := &myWriter{}
+	WriteData(w, []byte("привет\n")) // Запишем данные в слайс
+	fmt.Println(w.String()) // "привет\n"
+}*/

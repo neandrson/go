05ba320346727858ecf8724f89e5e37536839260
neandrson <a2361@yandex.ru>
2025-01-01 20:36:09 +0300

    Update main.go

--- 1_/1.2.SortSliceString/main.go
+++ 1_/1.2.SortSliceString/main.go
@@ -6,10 +6,15 @@ import (
 )
 
 func SortNames(names []string) {
+	// Пример 1
 	//sort.Sort(sort.StringSlice(names))
+
+	//Пример 2
 	/*sort.Slice(names, func(i1, i2 int) bool {
 		return len(names[i1]) > len(names[i2])
 	})*/
+
+	//Пример 3
 	sort.Strings(names)
 
 	fmt.Println(names)

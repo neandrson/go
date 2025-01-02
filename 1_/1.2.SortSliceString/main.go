d00e4a0400c8bed7bedde217f30913612b6ccddc
neandrson <a2361@yandex.ru>
2025-01-02 23:31:44 +0300

    Update main.go

--- 1_/1.2.SortSliceString/main.go
+++ 1_/1.2.SortSliceString/main.go
@@ -7,18 +7,22 @@ import (
 
 func SortNames(names []string) {
 	// Пример 1
-	sort.Sort(sort.StringSlice(names))
-	user := names[:len(names)-1]
-	fmt.Print(user)
+	//sort.Sort(sort.StringSlice(names))
+	//user := names[:len(names)-1]
+	//fmt.Print(names)
 
 	//Пример 2
-	/*sort.Slice(names, func(i1, i2 int) bool {
-		return len(names[i1]) > len(names[i2])
-	})*/
+	/*sort.Slice(names, func(i, j int) bool {
+		return len(names[i]) < len(names[j])
+	})
+	fmt.Print(names)*/
 
 	//Пример 3
-	/*sort.Strings(names)
-	j := len(names)
+	sort.Strings(names)
+	var user []string = names
+	user = names[:len(names)-1]
+	fmt.Print(user)
+	/*j := len(names)
 	for i := range names {
 		fmt.Print(names[i])
 		j--

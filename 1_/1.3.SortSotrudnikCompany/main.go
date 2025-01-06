a62fe2f7643018b2f7d260b4c0fa4aa1e22b3920
neandrson <a2361@yandex.ru>
2025-01-06 21:54:01 +0300

    Update main.go

--- 1_/1.3.SortSotrudnikCompany/main.go
+++ 1_/1.3.SortSotrudnikCompany/main.go
@@ -9,30 +9,37 @@ type CompanyInterface interface {
 	SortWorkers() ([]string, error)
 }
 
-type AddWorkerInfo struct {
-	Name, Position     []string
-	Salary, Experience []uint
+type Worker struct {
+	Name       string
+	Position   string
+	Salary     uint
+	Experience uint
 }
 
-func SortWorkers(Salary []uint, Position []string) []string {
-	return "" //sort.Sort(string(Salary)) + sort.Sort(Position)
+func (w Worker) AddWorkerInfo(workers []Worker) error {
+	//var workerSlice []string
+	/*for _, result := range worker {
+		workerSlice = append(workerSlice, string(result))
+	}*/
+	fmt.Println(w.Name, w.Position, w.Salary, w.Experience)
+	return nil
 }
 
-func (a AddWorkerInfo) AddWorkerInfo(Name, Position []string, Salary, Experience []uint) {
-	fmt.Println(Name, Position, Salary, Experience)
+func SortWorkers(workers []string) ([]string, error) {
+	return workers, nil //sort.Sort(string(Salary)) + sort.Sort(Position)
 }
 
 func main() {
-	workers := []AddWorkerInfo{
+	workers := []Worker{
 		{Name: "Михаил", Position: "директор", Salary: 200, Experience: 5},
 		{Name: "Игорь", Position: "зам. директора", Salary: 180, Experience: 3},
 		{Name: "Николай", Position: "начальник цеха", Salary: 120, Experience: 2},
 		{Name: "Андрей", Position: "мастер", Salary: 90, Experience: 10},
 		{Name: "Виктор", Position: "рабочий", Salary: 80, Experience: 3},
 	}
-	workersSort, err := AddWorkerInfo(workers)
-	if err != nil {
-		fmt.Println("")
-	}
-	fmt.Println(workersSort)
+	worker, _ := AddWorkerInfo(workers)
+	/*if err != nil {
+		fmt.Println("Ошибка")
+	}*/
+	fmt.Println(worker)
 }

e338581c73ec4f3a82eb1b9f5596e6872fde655a
neandrson <a2361@yandex.ru>
2025-01-09 22:36:03 +0300

    Create main.go

---
+++ 1_/1.5.SortByFreq/main.go
@@ -0,0 +1,16 @@
+package main
+
+func SortByFreq(str string) string {
+	count := 0
+	for _, string = range str {
+		// Check for vowel
+		if str[i] == ch {
+			count++
+		}
+	}
+
+}
+
+func main() {
+
+}

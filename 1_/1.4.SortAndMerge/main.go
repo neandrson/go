7fa229dc4fb3e2a56d8325ecb52edfa14730f23d
neandrson <a2361@yandex.ru>
2025-01-09 22:36:01 +0300

    Update main.go

--- 1_/1.4.SortAndMerge/main.go
+++ 1_/1.4.SortAndMerge/main.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
-/*type Chislo struct {
-	Left  []int
-	Right []int
+/*
+	type Chislo struct {
+		Left  []int
+		Right []int
+	}
+*/
+/*type Num struct {
+	left  []int
+	right []int
 }*/
 
 func SortAndMerge(left, right []int) []int {
 	final := []int{}
+	sort.Ints(left)
+	sort.Ints(right)
 	i := 0
 	j := 0
 	for i < len(left) && j < len(right) {
@@ -26,16 +37,17 @@ func SortAndMerge(left, right []int) []int {
 	for ; j < len(right); j++ {
 		final = append(final, right[j])
 	}
+	//sort.Ints(left)
+	//sort.Ints(right)
+	//final = copy(left, right)
+
 	return final
 }
 
 func main() {
-	mergeds := []int{
-		left:  []int{490, 741, 88, 1, 10, 7, 234, 2234, 64, -12, 778, 21234, 4345, 45673, -23, 5, 78, 2, 1, 5},
-		right: []int{-1, 4, 5, 104, 1, 18733, 0},
-	}
-	//for _, merged := range mergeds {
-	a := SortAndMerge(mergeds)
-	//}
+	left := []int{490, 741, 88, 1, 10, 7, 234, 2234, 64, -12, 778, 21234, 4345, 45673, -23, 5, 78, 2, 1, 5}
+	right := []int{-1, 4, 5, 104, 1, 18733, 0}
+	a := SortAndMerge(left, right)
+
 	fmt.Println(a)
 }

17fb5ef23d2c855ff2e5f443538a3fa5930f0624
neandrson <a2361@yandex.ru>
2025-01-12 17:06:39 +0300

    Update main.go

--- 1_2/1.2.2.Knapsack/main.go
+++ 1_2/1.2.2.Knapsack/main.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 type Chest struct {
 	val []int
 	wt  []int
 }
 
 func Knapsack(chest *Chest, maxWeight int) (int, []int) {
-	n := len(chest.cost) // Количество драгоценностей
+	n := len(chest.val) // Количество драгоценностей
+	// Выделим память под слайсы
 	matrix := make([][]int, n+1)
 	for i := range matrix {
 		matrix[i] = make([]int, maxWeight+1)
@@ -18,10 +21,10 @@ func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 			maxcostWithoutCurrent := matrix[item-1][capacity] //  Максимальная стоимость предыдущих предметов
 			maxcostWithCurrent := 0                           //  Для хранения максимальной стоимости, если положим текущий предмет
 
-			weightOfCurrent := chest.mass[item-1] // Масса текущего
-			if capacity >= weightOfCurrent {      // Проверяем, влезет ли текущий предмет в рюкзак
+			weightOfCurrent := chest.wt[item-1] // Масса текущего
+			if capacity >= weightOfCurrent {    // Проверяем, влезет ли текущий предмет в рюкзак
 				// Если текущий влез, смотрим, что ещё взять
-				maxcostWithCurrent = chest.cost[item-1]                 // Сначала положим текущий предмет
+				maxcostWithCurrent = chest.val[item-1]                  // Сначала положим текущий предмет
 				remainingCapacity := capacity - weightOfCurrent         // Проверим, осталось ли место
 				maxcostWithCurrent += matrix[item-1][remainingCapacity] // Максимальная стоимость оставшегося места
 			}
@@ -29,20 +32,15 @@ func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 			matrix[item][capacity] = max(maxcostWithoutCurrent, maxcostWithCurrent) // Выбираем, нужно ли класть текущий
 		}
 	}
-	return matrix[n][maxWeight]
+	return n, matrix[0]
 }
 
 func main() {
 	w := 10 // Грузоподъёмность рюкзака
 	chest := Chest{
-		cost: []int{100, 400, 300, 500}, // Стоимость
-		mass: []int{5, 4, 6, 3},         // Масса
+		val: []int{100, 400, 300, 500}, // Стоимость
+		wt:  []int{5, 4, 6, 3},         // Масса
 	}
-	n := len(chest.cost) // Количество драгоценностей
-
-	// Выделим память под слайсы
-	matrix := make([][]int, n+1)
-	for i := range matrix {
-		matrix[i] = make([]int, w+1)
-	}
+	cost, items := Knapsack(chest, w)
+	fmt.Println(cost, items)
 }

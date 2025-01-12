35bcb8d115f2958a47283c50e2ae94cce0e488b7
neandrson <a2361@yandex.ru>
2025-01-12 16:08:00 +0300

    Create main.go

---
+++ 1_2/1.2.2.Knapsack/main.go
@@ -0,0 +1,48 @@
+package main
+
+type Chest struct {
+	val []int
+	wt  []int
+}
+
+func Knapsack(chest *Chest, maxWeight int) (int, []int) {
+	n := len(chest.cost) // Количество драгоценностей
+	matrix := make([][]int, n+1)
+	for i := range matrix {
+		matrix[i] = make([]int, maxWeight+1)
+	}
+
+	for item := 1; item <= n; item++ { // Переберём все предметы из сундука
+		for capacity := 1; capacity <= maxWeight; capacity++ {
+			// Всё ниже — о рюкзаке вместимостью capacity
+			maxcostWithoutCurrent := matrix[item-1][capacity] //  Максимальная стоимость предыдущих предметов
+			maxcostWithCurrent := 0                           //  Для хранения максимальной стоимости, если положим текущий предмет
+
+			weightOfCurrent := chest.mass[item-1] // Масса текущего
+			if capacity >= weightOfCurrent {      // Проверяем, влезет ли текущий предмет в рюкзак
+				// Если текущий влез, смотрим, что ещё взять
+				maxcostWithCurrent = chest.cost[item-1]                 // Сначала положим текущий предмет
+				remainingCapacity := capacity - weightOfCurrent         // Проверим, осталось ли место
+				maxcostWithCurrent += matrix[item-1][remainingCapacity] // Максимальная стоимость оставшегося места
+			}
+
+			matrix[item][capacity] = max(maxcostWithoutCurrent, maxcostWithCurrent) // Выбираем, нужно ли класть текущий
+		}
+	}
+	return matrix[n][maxWeight]
+}
+
+func main() {
+	w := 10 // Грузоподъёмность рюкзака
+	chest := Chest{
+		cost: []int{100, 400, 300, 500}, // Стоимость
+		mass: []int{5, 4, 6, 3},         // Масса
+	}
+	n := len(chest.cost) // Количество драгоценностей
+
+	// Выделим память под слайсы
+	matrix := make([][]int, n+1)
+	for i := range matrix {
+		matrix[i] = make([]int, w+1)
+	}
+}

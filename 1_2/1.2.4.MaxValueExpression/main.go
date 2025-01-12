f7713fff88e8eb328309043a29a236a7cefd56d7
neandrson <a2361@yandex.ru>
2025-01-12 12:34:30 +0300

    Update main.go

--- 1_2/1.2.4.MaxValueExpression/main.go
+++ 1_2/1.2.4.MaxValueExpression/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{5, 7, 9, 8, 4, 6, 3, 2, 1}
+	first := make([]int, len(nums)+1)
+	for i := len(nums) - 1; i >= 0; i-- {
+		first[i] = max(first[i+1], nums[i]) // Функция max — возвращает максимальное
+	}
+
+	second := make([]int, len(nums))
+	for i := len(nums) - 2; i >= 0; i-- {
+		second[i] = max(second[i+1], first[i+1]-nums[i])
+	}
+	// Здесь будет максимум
+	fmt.Println(second[0])
 }

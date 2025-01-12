b19a41fa4aebf1f838022366a4e1a5ff140bc9cd
neandrson <a2361@yandex.ru>
2025-01-12 17:58:10 +0300

    Update main.go

--- 1_2/1.2.3.MinPizzaCost/main.go
+++ 1_2/1.2.3.MinPizzaCost/main.go
@@ -3,19 +3,17 @@ package main
 import "fmt"
 
 func MinPizzaCost(s, m, l, cs, cm, cl, x int) int {
-	for _, coin := range coins {
-		// Переберём все значения слайса с количеством способов
-		for j := range ways {
-			// Если монета меньше значения, которое нам нужно получить
-			if coin <= j {
-				// Обновим значение с учётом предыдущих расчётов
-				ways[j] += ways[j-coin]
-			}
-		}
-	}
-	fmt.Println(ways[N])
+	
 }
 
 func main() {
-
+	s := 314
+	m := 706
+	l := 1256
+	cs := 230
+	cm := 510
+	cl := 925
+	x := 500
+	a := MinPizzaCost(s, m, l, cs, cm, cl, x)
+	fmt.Println(a)
 }

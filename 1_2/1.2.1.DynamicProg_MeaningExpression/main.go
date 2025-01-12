fa200534d039f815e374595bdd99cae5c964abe0
neandrson <a2361@yandex.ru>
2025-01-12 15:48:11 +0300

    Update main.go

--- 1_2/1.2.1.DynamicProg_MeaningExpression/main.go
+++ 1_2/1.2.1.DynamicProg_MeaningExpression/main.go
@@ -10,14 +10,24 @@ func MaxExpressionValue(nums []int) int {
 
 	second := make([]int, len(nums))
 	for i := len(nums) - 2; i >= 0; i-- {
-		second[i] = max(second[i+2], first[i+2]+nums[i+1]-nums[i])
+		second[i] = max(second[i+1], first[i+1]-nums[i])
+	}
+
+	thry := make([]int, len(nums))
+	for i := len(nums) - 2; i >= 0; i-- {
+		thry[i] = max(thry[i+1], second[i+1]+nums[i])
+	}
+
+	fhor := make([]int, len(nums))
+	for i := len(nums) - 2; i >= 0; i-- {
+		fhor[i] = max(fhor[i+1], thry[i+1]-nums[i])
 	}
 	// Здесь будет максимум
-	return second[0]
+	return fhor[0]
 }
 
 func main() {
-	nums := []int{3, 9, 10, 1, 30, 40}
+	nums := []int{4, 5, 10, 50, 25}
 	a := MaxExpressionValue(nums)
 	fmt.Println(a)
 }

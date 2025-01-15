2c7f21789e6c83ca71d54b86f23fbbba967b1c95
neandrson <a2361@yandex.ru>
2025-01-15 12:58:02 +0300

    Update main.go

--- 1_1/1.3.SortSotrudnikCompany/main.go
+++ 1_1/1.3.SortSotrudnikCompany/main.go
@@ -12,17 +12,17 @@ type CompanyInterface interface {
 }
 
 type Worker struct {
-	Name       string
-	Position   string
-	Salary     uint
-	Experience uint
+	Name            string
+	Position        string
+	Salary          uint
+	ExperienceYears uint
 }
 
-func AddWorkerInfo(workers Worker) error {
+func (w *Worker) AddWorkerInfo(Name, Position string, Salary, Experience uint) error {
 	var worker []string
 	var sum uint = 0
-	sum = workers.Salary * workers.Experience * 12
-	worker = append(worker, workers.Name+" - "+strconv.FormatUint(uint64(sum), 10)+" - "+workers.Position)
+	sum = w.Salary * w.ExperienceYears * 12
+	worker = append(worker, w.Name+" - "+strconv.FormatUint(uint64(sum), 10)+" - "+w.Position)
 	fmt.Println(strings.Trim(fmt.Sprintf("%v", worker), "[]"))
 	return nil
 }
@@ -33,16 +33,12 @@ func SortWorkers(workers []string) ([]string, error) {
 
 func main() {
 	workers := []Worker{
-		{Name: "Михаил", Position: "директор", Salary: 200, Experience: 5},
-		{Name: "Игорь", Position: "зам. директора", Salary: 180, Experience: 3},
-		{Name: "Николай", Position: "начальник цеха", Salary: 120, Experience: 2},
-		{Name: "Андрей", Position: "мастер", Salary: 90, Experience: 10},
-		{Name: "Виктор", Position: "рабочий", Salary: 80, Experience: 3},
+		{Name: "Михаил", Position: "директор", Salary: 200, ExperienceYears: 5},
+		{Name: "Игорь", Position: "зам. директора", Salary: 180, ExperienceYears: 3},
+		{Name: "Николай", Position: "начальник цеха", Salary: 120, ExperienceYears: 2},
+		{Name: "Андрей", Position: "мастер", Salary: 90, ExperienceYears: 10},
+		{Name: "Виктор", Position: "рабочий", Salary: 80, ExperienceYears: 3},
 	}
-	for _, worker := range workers {
-		err := AddWorkerInfo(worker)
-		if err != nil {
-			fmt.Println("Ошибка")
-		}
-	}
+	worker := workers.AddWorkerInfo(Name, Position, Salary, ExperienceYears)
+	fmt.Println(worker)
 }

2d0bb91dc7262ec999672f0e86255503bc65b1d7
neandrson <a2361@yandex.ru>
2025-01-22 22:28:53 +0300

    Create main.go

---
+++ 2/2.2.3.SendToCanall/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func Send(ch1, ch2 chan int) {
+	for i := 0; i < 3; i++ {
+		go func() {
+			ch1 <- i // отправляем значение в канал
+		}()
+		//time.Sleep(1 * time.Second)
+		go func() {
+			ch2 <- i // отправляем значение в канал
+		}()
+		//time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	Send(ch1, ch2)
+
+	vals := []int{}
+
+	for i := 0; i < 3; i++ {
+		val := <-ch1
+		vals = append(vals, val)
+	}
+
+	slices.Sort(vals)
+
+	expected := []int{0, 1, 2}
+	if !slices.Equal(vals, expected) {
+		fmt.Printf("ch1 values expected: %v, got: %v\n", expected, vals)
+	} else {
+		fmt.Printf("ch1 values got: %v\n", vals)
+	}
+}

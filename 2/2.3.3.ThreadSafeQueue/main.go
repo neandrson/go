a983455f4a3e632b198602f40afb38c0a17a15e0
neandrson <a2361@yandex.ru>
2025-01-25 11:49:40 +0300

    Create main.go

---
+++ 2/2.3.3.ThreadSafeQueue/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Queue interface {
+	Enqueue(element interface{}) // положить элемент в очередь
+	Dequeue() interface{}        // забрать первый элемент из очереди
+}
+
+type ConcurrentQueue struct {
+	queue []interface{} // здесь хранить элементы очереди
+	mutex sync.Mutex
+}
+
+func (c *ConcurrentQueue) Enqueue(element interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.queue = append(c.queue, element) // теперь доступ к мапе внутри критической секции
+}
+
+func (c *ConcurrentQueue) Dequeue() interface{} {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	return c.queue[:1]
+}
+
+func main() {
+	concurrentQueue := &ConcurrentQueue{}
+
+	Cases := []struct {
+		name     string
+		elements []interface{}
+	}{
+		{
+			name:     "Case 1",
+			elements: []interface{}{1, 2, 3},
+		},
+		{
+			name:     "Case 2",
+			elements: []interface{}{"a", "b", "c"},
+		},
+	}
+
+	for _, c := range Cases {
+		c := c
+		//Run(c.name, func(t *testing.T) {
+
+		for _, element := range c.elements {
+			concurrentQueue.Enqueue(element)
+		}
+
+		for _, expected := range c.elements {
+			actual := concurrentQueue.Dequeue()
+			if actual != expected {
+				fmt.Printf("Unexpected element. Got: %v, Expected: %v\n", actual, expected)
+			}
+		}
+
+		var wg sync.WaitGroup
+
+		for i := 1; i <= 10000; i++ {
+			wg.Add(1)
+			go func(item int) {
+				concurrentQueue.Enqueue(item)
+				wg.Done()
+			}(i)
+		}
+		wg.Wait()
+
+		if len(concurrentQueue.queue) != 10000 {
+			fmt.Printf("Unexpected len. Got: %v, Expected: %v\n", len(concurrentQueue.queue), len(c.elements))
+		}
+		//}
+	}
+}

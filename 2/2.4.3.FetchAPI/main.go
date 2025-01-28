c14b3debd58344c12607db6340b9120bae6bb08d
neandrson <a2361@yandex.ru>
2025-01-28 21:59:19 +0300

    Update main.go

--- 2/2.4.3.FetchAPI/main.go
+++ 2/2.4.3.FetchAPI/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"sync"
 	"time"
@@ -12,6 +13,20 @@ type APIResponse struct {
 	Data       string // тело ответа
 	StatusCode int    // код ответа
 	Err        error  // ошибка, если возникла
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func hiHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Microsecond * 100)
+	fmt.Fprintf(w, "Hi, World!")
+}
+
+func longHanlder(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func FetchAPI(ctx context.Context, urls []string, timeout time.Duration) []*APIResponse {
@@ -29,16 +44,17 @@ func FetchAPI(ctx context.Context, urls []string, timeout time.Duration) []*APIR
 			var client = http.Client{}
 			if err != nil {
 				mu.Lock()
-				//defer mu.Unlock()
+				defer mu.Unlock()
 				APRS = append(APRS, &APIResponse{URL: u, Err: err})
-				mu.Unlock()
+				//mu.Unlock()
 				return
 			}
 			resp, err := client.Do(req)
 			if err != nil {
 				mu.Lock()
+				defer mu.Unlock()
 				APRS = append(APRS, &APIResponse{URL: u, Data: "", StatusCode: resp.StatusCode, Err: err})
-				mu.Unlock()
+				//mu.Unlock()
 				return
 			}
 			defer resp.Body.Close()
@@ -50,4 +66,40 @@ func FetchAPI(ctx context.Context, urls []string, timeout time.Duration) []*APIR
 	}
 	wg.Wait()
 	return APRS
+}
+
+func main() {
+	ctx := context.Background()
+	go func() {
+		http.HandleFunc("/hello", helloHandler)
+
+		http.HandleFunc("/long", longHanlder)
+
+		http.HandleFunc("/hi", hiHandler)
+
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			fmt.Printf("error when starting a server\n")
+		}
+	}()
+
+	urls := []string{"http://localhost:8080/hello", "http://localhost:8080/hi", "http://localhost:8080/long"}
+	timeout := 2 * time.Second
+
+	client := &http.Client{}
+	time.Sleep(500 * time.Millisecond)
+	go FetchAPI(ctx, urls, timeout)
+	req, err := http.NewRequest("GET", url, nil)
+	// добавляем заголовки
+	//req.Header.Add("Accept", "text/html")   // добавляем заголовок Accept
+	//req.Header.Add("User-Agent", "MSIE/15.0")   // добавляем заголовок User-Agent
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Printf("resp, %w", resp)
+	}
+	defer resp.Body.Close()
 }

2bd9eb7d8f8b11cb7a0bea6dce9edd8610014246
neandrson <a2361@yandex.ru>
2025-01-30 11:41:55 +0300

    Update main.go

--- 2/2.5_prim_MutexRWMutex/main.go
+++ 2/2.5_prim_MutexRWMutex/main.go
@@ -7,12 +7,46 @@ import (
 	"time"
 )
 
+// Пример 4
+type SafeSlice struct {
+	results []int
+	mx      *sync.Mutex
+}
+
+// добавляем к слайсу элемент item
+func (s *SafeSlice) Append(item int) {
+	// вызван Lock, поэтому только одна горутина за раз может получить доступ к
+	// слайсу
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	s.results = append(s.results, random())
+}
+
+// получаем элемент слайса по индексу
+func (s *SafeSlice) Get(index int) int {
+	// вызван Lock, поэтому только одна горутина за раз может получить доступ к
+	// слайсу
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	return s.results[index]
+}
+
+// создаёт экземпляр нашей структуры, для удобства пользователя
+func New() *SafeSlice {
+	return &SafeSlice{
+		mx:      &sync.Mutex{},
+		results: []int{},
+	}
+}
+
+// Пример 1
 // функция генерирует случайное число в интервале [0, 100)
 func random() int {
 	const max int = 100
 	return rand.Intn(max)
 }
 
+// Пример 1
 func main() {
 	const size int = 10
 	results := []int{}
@@ -29,6 +63,7 @@ func main() {
 			mx.Lock()
 			defer mx.Unlock()
 
+			// Пример 2
 			results = append(results, random())
 		}()
 	}
@@ -41,6 +76,7 @@ func main() {
 	mx.Lock()
 	defer mx.Unlock()
 
+	// Пример 1
 	// поэлементно выводим слайс на экран
 	for i := 0; i < size; i++ {
 		fmt.Println(results[i])

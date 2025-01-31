6dc9d329fe14b8492497e5b7de4d58888d275375
neandrson <a2361@yandex.ru>
2025-01-31 21:37:47 +0300

    Create main.go

---
+++ 1_4/1.4_prim_SetState/cmd/life/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/neandrson/go/1_4/internal/application"
+)
+
+func main() {
+	ctx := context.Background()
+	// Exit приводит к завершению программы с заданным кодом
+	os.Exit(mainWithExitCode(ctx))
+}
+
+func mainWithExitCode(ctx context.Context) int {
+	cfg := application.Config{
+		Width:  100,
+		Height: 100,
+	}
+	app := application.New(cfg)
+
+	return app.Run(ctx)
+}

53799db3faff53e6c4a809522594dc2682c03dd0
neandrson <a2361@yandex.ru>
2025-01-31 21:38:01 +0300

    Create service.go

---
+++ 1_4/1.4_prim_SetState/internal/service/service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"math/rand"
+	"time"
+	//"golang.org/x/exp/rand"
+)
+
+// Для хранения состояния
+type LifeService struct {
+	currentWorld *life.World
+	nextWorld    *life.World
+}
+
+func New(height, width int) (*LifeService, error) {
+	rand.NewSource(time.Now().UTC().UnixNano())
+
+	currentWorld, err := life.NewWorld(height, width)
+	if err != nil {
+		return nil, err
+	}
+	// Заполним случайными показателями, чтобы упростить пример
+	currentWorld.RandInit(40)
+
+	newWorld, err := life.NewWorld(height, width)
+	if err != nil {
+		return nil, err
+	}
+
+	ls := LifeService{
+		currentWorld: currentWorld,
+		nextWorld:    newWorld,
+	}
+
+	return &ls, nil
+}
+
+// Получаем очередное состояние игры
+func (ls *LifeService) NewState() *life.World {
+	life.NextState(ls.currentWorld, ls.nextWorld)
+
+	ls.currentWorld = ls.nextWorld
+
+	return ls.currentWorld
+}

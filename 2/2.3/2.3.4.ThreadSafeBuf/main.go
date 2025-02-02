09020dc27c22a7be25e798c5d3881456dbd1d6f4
neandrson <a2361@yandex.ru>
2025-02-02 21:29:05 +0300

    Create main.go

---
+++ 2/2.3/2.3.4.ThreadSafeBuf/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var mu sync.Mutex
+var Buf []int
+
+func Write(num int) {
+	mu.Lock()
+	defer mu.Unlock()
+	Buf = append(Buf, num)
+}
+
+func Consume() int {
+	mu.Lock()
+	defer mu.Unlock()
+	/*if len(c.queue) == 0 {
+		return nil
+	}*/
+	buf := Buf[0]
+	Buf = Buf[1:]
+	return buf
+}
+
+func main() {
+	Buf := []int{}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			Write(i)
+		}()
+	}
+
+	wg.Wait()
+
+	if len(Buf) != 1000 {
+		fmt.Printf("Unexpected len. Got: %d, Expected: %d\n", len(Buf), 1000)
+	} else {
+		fmt.Printf("Unexpected len. Got: %d\n", len(Buf))
+	}
+
+	var results []int
+
+	var resultMu sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			res := Consume()
+
+			resultMu.Lock()
+			results = append(results, res)
+			resultMu.Unlock()
+		}()
+	}
+	wg.Wait()
+
+	if len(Buf) != 0 {
+		fmt.Printf("Unexpected len. Got: %d, Expected: %d\n", len(Buf), 0)
+	} else {
+		fmt.Printf("Unexpected len. Got: %d\n", len(Buf))
+	}
+	if len(results) != 1000 {
+		fmt.Printf("Unexpected results len. Got: %d, Expected: %d\n", len(results), 1000)
+	} else {
+		fmt.Printf("Unexpected results len. Got: %d\n", len(results))
+	}
+}

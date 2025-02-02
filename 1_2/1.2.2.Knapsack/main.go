223350fda81dfe230f8ba9f7883ada37732a7dba
neandrson <a2361@yandex.ru>
2025-02-02 14:12:49 +0300

    Update main.go

--- 1_2/1.2.2.Knapsack/main.go
+++ 1_2/1.2.2.Knapsack/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Chest struct {
 	val []int
 	wt  []int
 }
 
-func Knapsack(chest *Chest, maxWeight int) int {
+/*func Knapsack(chest *Chest, maxWeight int) (int, int) {
 	n := len(chest.val) // Количество драгоценностей
 	//k := 0
 	// Выделим память под слайсы
@@ -32,7 +35,7 @@ func Knapsack(chest *Chest, maxWeight int) int {
 		}
 	}
 	fmt.Println("the dp array is", matrix)
-	return matrix[len(chest.wt)][maxWeight]
+	return chest.wt[len(chest.wt)-1], matrix[len(chest.wt)][maxWeight]
 }
 
 func max(a, b int) int {
@@ -49,6 +52,45 @@ func main() {
 		val: []int{100, 400, 300, 500}, // Стоимость
 		wt:  []int{5, 4, 6, 3},         // Масса
 	}
-	items := Knapsack(&chest, w)
-	fmt.Println(items)
+	wt, les := Knapsack(&chest, w)
+	fmt.Println(wt, les)
+}*/
+
+func Knapsack(chest *Chest, maxWeight int) (int, []int) {
+	// рассчет стоимости
+	n := len(chest.val)
+	m := maxWeight
+	dp1 := make([]int, n+1)
+	// create dp data structure
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 0; i < len(chest.wt); i++ {
+		for j := 0; j <= maxWeight; j++ {
+			if chest.wt[i] > j {
+				dp[i+1][j] = dp[i][j]
+				dp1[i+1] = i
+			} else {
+				dp[i+1][j] = Max(dp[i][j-chest.wt[i]]+chest.val[i], dp[i][j])
+
+			}
+		}
+	}
+	fmt.Println(dp, dp1)
+	return dp[n][m], dp1
+}
+
+func Max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func main() {
+	w := 10 // Грузоподъёмность рюкзака
+	chest := Chest{
+		val: []int{100, 400, 300, 500}, // Стоимость
+		wt:  []int{5, 4, 6, 3},         // Масса
+	}
+	maxProfit, item := Knapsack(&chest, w)
+	fmt.Println(maxProfit, item)
 }

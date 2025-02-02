a9dcbe5d867486734940b8184c64233c7b289f0c
neandrson <a2361@yandex.ru>
2025-02-02 19:31:15 +0300

    Create main.go

---
+++ 2/2.4/2.4.1.Contains/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+)
+
+func Contains(ctx context.Context, r io.Reader, seq []byte) (bool, error) {
+	if len(seq) == 0 {
+		return true, errors.New("sequence cannot be empty")
+	}
+
+	buf := make([]byte, 1000)
+	window := make([]byte, 0, len(seq))
+
+	for {
+		select {
+		case <-ctx.Done():
+			return false, ctx.Err()
+		default:
+
+			n, err := r.Read(buf)
+			if err != nil && !errors.Is(err, io.EOF) {
+				return false, err
+			}
+
+			window = append(window, buf[:n]...)
+
+			if bytes.Contains(window, seq) {
+				return true, nil
+			}
+			if err != nil && err == io.EOF {
+				return false, nil
+			}
+
+			if len(window) > len(seq) {
+				window = window[len(window)-len(seq):]
+			}
+		}
+		buff := append(buf[1:], 0) // Сдвиг в буфере
+		_, err := r.Read(buff[len(buff)-1:])
+		if err != nil {
+			return false, nil
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	r := bytes.NewReader([]byte{0x61, 0x64, 0x73, 0x61, 0x64, 0x73, 0x61, 0x64, 0x73})
+	fmt.Println(Contains(ctx, r, []byte{0x61, 0x64, 0x73})) // true <nil>
+	fmt.Println(Contains(ctx, r, []byte{0x61, 0x67, 0x73})) // false <nil>
+}

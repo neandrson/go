3a1229e1f664d5dcb177315a553c5cd34f427070
neandrson <a2361@yandex.ru>
2025-02-03 19:58:21 +0300

    Update main.go

---
+++ 2/2.8/2.8.3.TestReverseString/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func ReverseString(input string) string {
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	str := "The Primer ReverseString"
+
+	rstr := ReverseString(str)
+	fmt.Println(rstr)
+}

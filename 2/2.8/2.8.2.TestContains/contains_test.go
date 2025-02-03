baacbc44f1886e0c6e3830e0b8d5660905dcaf2e
neandrson <a2361@yandex.ru>
2025-02-03 11:43:52 +0300

    Create contains_test.go

---
+++ 2/2.8/2.8.2.TestContains/contains_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestContains(t *testing.T) {
+
+	testsCases := []struct {
+		name    string
+		numbers []int
+		target  int
+		want    bool
+		err     error
+	}{
+		{
+			name:    "Test1",
+			numbers: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			target:  5,
+			want:    true,
+			//err:  errors.New("tmp"),
+		},
+		{
+			name:    "Test2",
+			numbers: []int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			target:  0,
+			want:    false,
+			//err:  errors.New("tmp"),
+		},
+	}
+
+	for _, tc := range testsCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			got := Contains(tc.numbers, tc.target)
+			if !got {
+				t.Errorf("Expected boolean:, %f, got: %f\n", tc.want, got)
+			}
+
+		})
+	}
+}

8b4a476109666b54d1089c2a71b342f22e8f4607
neandrson <a2361@yandex.ru>
2025-02-05 20:51:22 +0300

    Update main.go

--- 1_2/1.2.2.Knapsack/main.go
+++ 1_2/1.2.2.Knapsack/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 type Chest struct {
@@ -10,41 +9,64 @@ type Chest struct {
 	wt  []int
 }
 
-/*func Knapsack(chest *Chest, maxWeight int) (int, int) {
+func Knapsack(chest *Chest, maxWeight int) int {
 	n := len(chest.val) // Количество драгоценностей
-	//k := 0
-	// Выделим память под слайсы
 	matrix := make([][]int, n+1)
 	for i := range matrix {
 		matrix[i] = make([]int, maxWeight+1)
 	}
 
-	for i := chest.wt[0]; i <= maxWeight; i++ {
-		matrix[0][i] = chest.val[0]
-	}
+	for item := 1; item <= n; item++ { // Переберём все предметы из сундука
+		for capacity := 1; capacity <= maxWeight; capacity++ {
+			// Всё ниже — о рюкзаке вместимостью capacity
+			maxcostWithoutCurrent := matrix[item-1][capacity] //  Максимальная стоимость предыдущих предметов
+			maxcostWithCurrent := 0                           //  Для хранения максимальной стоимости, если положим текущий предмет
 
-	for i := 1; i < len(matrix); i++ {
-		for j := 1; j <= maxWeight; j++ {
-			for n := 0; n*chest.wt[i] <= j; n++ {
-				if chest.wt[i-1] <= j {
-					matrix[i][j] = max(matrix[i-1][j-n*chest.wt[i]]+n*chest.val[i], matrix[i][j])
-				} else {
-					matrix[i][j] = matrix[i-1][j]
-				}
+			weightOfCurrent := chest.wt[item-1] // Масса текущего
+			if capacity >= weightOfCurrent {    // Проверяем, влезет ли текущий предмет в рюкзак
+				// Если текущий влез, смотрим, что ещё взять
+				maxcostWithCurrent = chest.val[item-1]                  // Сначала положим текущий предмет
+				remainingCapacity := capacity - weightOfCurrent         // Проверим, осталось ли место
+				maxcostWithCurrent += matrix[item-1][remainingCapacity] // Максимальная стоимость оставшегося места
+			}
+
+			matrix[item][capacity] = max(maxcostWithoutCurrent, maxcostWithCurrent) // Выбираем, нужно ли класть текущий
+		}
+	}
+	fmt.Println(matrix)
+	item := GetSolution(maxWeight)
+	return matrix[n][maxWeight]
+}
+
+func GetSolution(maxWeight Weight, items []int, getWeight func(*T) Weight, getValue func(*T) Value) (selection []T) {
+	// Perform the dynamic programming 0-1 knapsack algorithm.
+	maxValue := make([]int, maxWeight+1)
+	for m := range items {
+		itemWeight := getWeight(&items[m])
+		itemValue := getValue(&items[m])
+		for weight := maxWeight; weight >= itemWeight; weight-- {
+			maxValueWithItem := itemValue + maxValue[weight-itemWeight].maxValue
+			if maxValueWithItem > maxValue[weight].maxValue {
+				maxValue[weight].maxValue = maxValueWithItem
+				maxValue[weight].selectedItems.Set(&maxValue[weight-itemWeight].selectedItems).Add(m)
 			}
 		}
 	}
-	fmt.Println("the dp array is", matrix)
-	return chest.wt[len(chest.wt)-1], matrix[len(chest.wt)][maxWeight]
+	selection = make([]T, maxValue[maxWeight].selectedItems.Size())[:0]
+	maxValue[maxWeight].selectedItems.Visit(func(index int) bool {
+		selection = append(selection, items[index])
+		return false
+	})
+	return
 }
 
-func max(a, b int) int {
+/*func max(a, b int) int {
 	if a > b {
 		return a
 	} else {
 		return b
 	}
-}
+}*/
 
 func main() {
 	w := 10 // Грузоподъёмность рюкзака
@@ -52,11 +74,11 @@ func main() {
 		val: []int{100, 400, 300, 500}, // Стоимость
 		wt:  []int{5, 4, 6, 3},         // Масса
 	}
-	wt, les := Knapsack(&chest, w)
-	fmt.Println(wt, les)
-}*/
+	wt := Knapsack(&chest, w)
+	fmt.Println(wt)
+}
 
-func Knapsack(chest *Chest, maxWeight int) (int, []int) {
+/*func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 	// рассчет стоимости
 	n := len(chest.val)
 	m := maxWeight
@@ -70,7 +92,7 @@ func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 		for j := 0; j <= maxWeight; j++ {
 			if chest.wt[i] > j {
 				dp[i+1][j] = dp[i][j]
-				/*select {
+				select {
 				case dp[i][j] == 100:
 					dp1[i] = 0
 				case dp[i][j] == 400:
@@ -79,7 +101,7 @@ func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 					dp1[i] = 2
 				default:
 					dp1[i] = 3
-				}*/
+				}
 				//dp1[i] = i
 
 			} else {
@@ -91,13 +113,13 @@ func Knapsack(chest *Chest, maxWeight int) (int, []int) {
 	}
 	fmt.Println(dp, dp1)
 	return dp[n][m], dp1
-}
+}*/
 
-func Max(a, b int) int {
+/*func Max(a, b int) int {
 	return int(math.Max(float64(a), float64(b)))
-}
+}*/
 
-func main() {
+/*func main() {
 	w := 10 // Грузоподъёмность рюкзака
 	chest := Chest{
 		val: []int{100, 400, 300, 500}, // Стоимость
@@ -105,4 +127,4 @@ func main() {
 	}
 	maxProfit, item := Knapsack(&chest, w)
 	fmt.Println(maxProfit, item)
-}
+}*/

cdc41e8c6e047971e4c3f0e6efd4762d6fd9e1ab
neandrson <a2361@yandex.ru>
2025-02-10 16:58:53 +0300

    Update main.go

--- 1_2/1.2.3.MinPizzaCost/main.go
+++ 1_2/1.2.3.MinPizzaCost/main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Chest struct {
+	val        []int
+	wt         []int
+	itemsToPut []int
+	cap        int
+	cost       int
+}
 
 func MinPizzaCost(s, m, l, cs, cm, cl, x int) int {
-	
+	vales := Chest{val: []int{s, m, l}, wt: []int{cs, cm, cl}}
+	// рассчет стоимости
+	n := len(vales.val)
+
+	// create dp data structure
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j <= n; j++ {
+			if vales.wt[i] > j {
+				dp[i+1][j] = dp[i][j]
+			} else {
+				dp[i+1][j] = min(dp[i][j-vales.wt[i]]+vales.val[i], dp[i][j])
+			}
+		}
+	}
+	fmt.Println(dp, n)
+	return dp[n][m]
 }
 
 func main() {

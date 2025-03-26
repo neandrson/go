2fa180683e6ff153bfcedba5bfeb561634b80a46
neandrson <a2361@yandex.ru>
2025-03-26 09:28:32 +0300

    Create main.go

---
+++ 3/3.1.module_3_lesson_1_1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+/*func Sum(a, b int) int {
+	return a + b
+}*/
+
+func Sum[T any](nums []T) T {
+	var total T
+	for _, n := range nums {
+		total += n
+	}
+	return total
+}
+
+func main() {
+	intSum := Sum([]int{1, 2, 3})
+	if intSum != 6 {
+		fmt.Printf("Expected Sum([]int{1, 2, 3}) to be 6, but got %d\n", intSum)
+	} else {
+		fmt.Printf("Sum([]int{1, 2, 3}) got %d\n", intSum)
+	}
+
+	floatSum := Sum([]float64{1.5, 2.5, 3.0})
+	if floatSum != 7.0 {
+		fmt.Printf("Expected Sum([]float64{1.5, 2.5, 3.0}) to be 7.0, but got %f\n", floatSum)
+	} else {
+		fmt.Printf("Sum([]float64{1.5, 2.5, 3.0}) got %f\n", floatSum)
+	}
+}

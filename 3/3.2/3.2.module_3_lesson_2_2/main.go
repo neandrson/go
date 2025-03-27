058f7475fb9dfc74e086eef92b7d9e3d7445077f
neandrson <a2361@yandex.ru>
2025-03-27 22:18:43 +0300

    Update main.go

--- 3/3.2/3.2.module_3_lesson_2_2/main.go
+++ 3/3.2/3.2.module_3_lesson_2_2/main.go
@@ -2,21 +2,16 @@ package main
 
 import "fmt"
 
-/*type any interface {
-	int | float64 | string
-}*/
-
 func ToString[T any](done <-chan struct{}, valueStream <-chan T) <-chan string {
-	out := make(chan string) // канал для записи выходных данных
-	go func() {              // запускаем в отдельной горутине
+	out := make(chan string, 3) // канал для записи выходных данных
+	go func() {                 // запускаем в отдельной горутине
 		defer close(out) // закроем канал, когда больше нет данных
 		for i := range valueStream {
 			select {
 			case <-done:
 				return // после закрытия канала done - выходим
 			default:
-				x := fmt.Sprint("%d", i)
-				out <- x // запишем в канал
+				out <- fmt.Sprint(i) // запишем в канал
 			}
 		}
 	}()

6d7c28ac4dc6fe441adfd0105584905f2d62ae1f
neandrson <a2361@yandex.ru>
2025-03-29 21:40:00 +0300

    Update main.go

--- 3/3.3/3.3.1.module_3_lesson_3_1/main.go
+++ 3/3.3/3.3.1.module_3_lesson_3_1/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"compress/gzip"
+	"io"
 	"io/fs"
+	"os"
 	"path/filepath"
 	"regexp"
+	"sync"
 )
 
 type Work struct {
@@ -11,18 +15,16 @@ type Work struct {
 }
 
 func FileNameGen(dir string, pattern *regexp.Regexp) <-chan Work {
-	jobs := make(chan Work, 10)
+	jobs := make(chan Work)
 	go func() {
-		defer close(jobs) // закроем канал после обхода всех файлов
-		// функция для перебора файлов в директории
+		defer close(jobs)
 		filepath.Walk(dir, func(path string, d fs.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			// пропускаем вложенные директории
-			if !d.IsDir() {
-				// запишем в канал файл, которые нужно обработать на следующем этапе
-				jobs <- Work{file: path, pattern: pattern}
+			yandexIssue := regexp.MustCompile("(output.txt|input.txt)")
+			if !yandexIssue.Match([]byte(path)) && !d.IsDir() && pattern.MatchString(path) {
+				jobs <- Work{FilePath: path}
 			}
 			return nil
 		})
@@ -31,9 +33,41 @@ func FileNameGen(dir string, pattern *regexp.Regexp) <-chan Work {
 }
 
 func compress(jobs <-chan Work) {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for w := range jobs {
+				file, err := os.Open(w.FilePath)
+				if err != nil {
+					panic(err)
+				}
 
+				fw, err := os.Create(w.FilePath + ".gz")
+				if err != nil {
+					panic(err)
+				}
+
+				cw := gzip.NewWriter(fw)
+				_, err = io.Copy(cw, file)
+
+				if err != nil {
+					panic(err)
+				}
+				fw.Close()
+				file.Close()
+				cw.Close()
+			}
+		}()
+	}
+	wg.Wait()
 }
 
 func main() {
+	dir := "c:\\"
+	pattern := `^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`
 
+	jobs := FileNameGen(dir, pattern)
+	fmt.Println(regexp.MatchString(jobs))
 }

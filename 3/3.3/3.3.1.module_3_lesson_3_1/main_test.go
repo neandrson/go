8b98222dbb9c9f975031cffff9b00467725f6b84
neandrson <a2361@yandex.ru>
2025-03-30 11:40:14 +0300

    Create main_test.go

---
+++ 3/3.3/3.3.1.module_3_lesson_3_1/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+	"slices"
+	"testing"
+)
+
+func TestCompress(t *testing.T) {
+	dir := "./"
+
+	testFiles := []struct {
+		desc        string
+		fileContent string
+		fileName    string
+	}{
+		{
+			desc:        "",
+			fileContent: "Test file 1",
+			fileName:    "file1.txt",
+		},
+		{
+			desc:        "",
+			fileContent: "Test file 2",
+			fileName:    "file2.txt",
+		},
+	}
+
+	for _, testFile := range testFiles {
+		path := filepath.Join(dir, testFile.fileName)
+		createTestFile(t, path, testFile.fileContent)
+	}
+
+	defer func() {
+		for _, testFile := range testFiles {
+			path := filepath.Join(dir, testFile.fileName)
+			pathGzip := filepath.Join(dir, testFile.fileName+".gz")
+			os.Remove(path)
+			os.Remove(pathGzip)
+		}
+	}()
+
+	// Define the file pattern to match
+	pattern := regexp.MustCompile(`\.txt$`)
+
+	// Generate a stream of files to compress
+	files := FileNameGen(dir, pattern)
+
+	// Compress the files in parallel
+	compress(files)
+
+	// Verify that the compressed 
+}

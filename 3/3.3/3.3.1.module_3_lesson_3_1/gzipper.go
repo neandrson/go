84560dd7abc1e53b8813d0d5877f86d3bc7a8cbd
neandrson <a2361@yandex.ru>
2025-04-01 17:01:30 +0300

    Update gzipper.go

--- 3/3.3/3.3.1.module_3_lesson_3_1/gzipper.go
+++ 3/3.3/3.3.1.module_3_lesson_3_1/gzipper.go
@@ -55,16 +55,87 @@ func compress(jobs <-chan Work) {
 		if err != nil {
 			continue
 		}
-		err = os.Remove(work.Filepath)
-		if err != nil {
-			continue
-		}
+		os.Remove(work.Filepath)
 	}
 }
 
+func createTestFile(path string, fileContent string) {
+	//limitReader := io.LimitReader(*testFile.Reader, testFile.FileSize)
+
+	f, err := os.Create(path)
+	if err != nil {
+		fmt.Errorf("file creation error: %v", err)
+		return
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Printf("f.Close() error:%+v\n", err)
+		}
+	}()
+
+	w := bufio.NewWriter(f)
+	buf := make([]byte, len(fileContent)-1)
+	for {
+		/*nBuf, err := limitReader.Read(buf)
+		if err == io.EOF {
+			break
+		}*/
+		// fmt.Printf("nBuf: %d, err: %+v, data: %s\n", nBuf, err, base64.StdEncoding.EncodeToString(buf))
+
+		if _, err = w.Write(buf[:len(fileContent)-1]); err != nil {
+			fmt.Errorf("writing temp file: %+v", err)
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		t.Errorf("flushing file: %+v", err)
+	}
+
+	//return
+}
+
 func main() {
-	dir := "C:\\"
-	pattern := `^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$`
+	dir := "./"
+
+	files := []struct {
+		desc        string
+		fileContent string
+		fileName    string
+	}{
+		{
+			desc:        "",
+			fileContent: "Test file 1",
+			fileName:    "file1.txt",
+		},
+		{
+			desc:        "",
+			fileContent: "Test file 2",
+			fileName:    "file2.txt",
+		},
+	}
+
+	for _, file := range files {
+		path := filepath.Join(dir, file.fileName)
+		createTestFile(path, file.fileContent)
+	}
+
+	defer func() {
+		for _, file := range files {
+			path := filepath.Join(dir, file.fileName)
+			pathGzip := filepath.Join(dir, file.fileName+".gz")
+			os.Remove(path)
+			os.Remove(pathGzip)
+		}
+	}()
+
+	// Define the file pattern to match
+	pattern := regexp.MustCompile("\\.txt$")
+
+	// Generate a stream of files to compress
+	fil := FileNameGen(dir, pattern)
+
+	// Compress the files in parallel
+	compress(files)
 
 	jobs := fs.ReadDir(dir, pattern)
 	fmt.Println(regexp.MatchString(jobs))

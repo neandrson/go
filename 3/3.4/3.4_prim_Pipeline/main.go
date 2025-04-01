ff71085a27d81726f104228a5ab2a9008182623e
neandrson <a2361@yandex.ru>
2025-04-01 20:31:40 +0300

    Update main.go

--- 3/3.4/3.4_prim_Pipeline/main.go
+++ 3/3.4/3.4_prim_Pipeline/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func EvenNumbersGen(numbers ...int) <-chan int {
 	out := make(chan int) // канал для записи выходных данных
@@ -26,6 +29,34 @@ func DoubleNumbers(in <-chan int) <-chan int {
 	return out // вернём канал
 }
 
+func merge(cs ...<-chan int) <-chan int {
+	// для синхронизации
+	var wg sync.WaitGroup
+	// объединять будем в этот канал
+	out := make(chan int)
+	// output - функция, которая копирует данные из
+	// входящего канала (пока он не закрыт) в out,
+	output := func(c <-chan int) {
+		defer wg.Done()
+		for n := range c {
+			out <- n
+		}
+	}
+	// добавим по количеству каналов
+	wg.Add(len(cs))
+	for _, c := range cs {
+		// запустим функцию output в отдельном канале
+		go output(c)
+	}
+	// запустим горутину, ожидающую завершения чтения данных из всех входящих
+	// каналов. Затем закроем выходящий канал
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
 func main() {
 	// Пример 1 -
 	// канал в четными числами
@@ -39,11 +70,22 @@ func main() {
 
 	// Пример 2 -
 	// канал в четными числами
-	evens := EvenNumbersGen(1, 2, 3, 4, 5, 6)
+	//evens := EvenNumbersGen(1, 2, 3, 4, 5, 6)
 	// канал с дважды удвоенными числами
-	out := DoubleNumbers(DoubleNumbers(evens))
+	//out := DoubleNumbers(DoubleNumbers(evens))
 	// печатаем каждое число
-	for num := range out {
-		fmt.Println(num) //8, 16, 24
+	//for num := range out {
+	//	fmt.Println(num) //8, 16, 24
+	//}
+
+	// Пример 3 -
+	// канал в четными числами
+	evens := EvenNumbersGen(1, 2, 3, 4, 5, 6)
+	// Распределим нагрузку. Обе функцию читают из одного канала
+	doubled1 := DoubleNumbers(evens)
+	doubled2 := DoubleNumbers(evens)
+	// Прочитаем значения с объединенного канала
+	for n := range merge(doubled1, doubled2) {
+		fmt.Println(n) // 4, 8, 12 - могут быть в любом порядке
 	}
 }

4adf1dbadd64f5e3161fb7301bb31579d1721341
neandrson <a2361@yandex.ru>
2025-04-01 08:05:24 +0300

    Update main.go

--- 3/3.3/3.3.2.module_3_lesson_3_2/main.go
+++ 3/3.3/3.3.2.module_3_lesson_3_2/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "io"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+	"strings"
+	"sync"
+)
 
 type WordCounter struct {
 	wordsCount map[string]int // здесь должен быть список слов с указанием количества повторений во всех файлах.
@@ -10,6 +17,40 @@ type WordCounter struct {
 type CounterWorker interface {
 	ProcessFiles(files ...string) error // для запуска обработки файлов
 	ProcessReader(r io.Reader) error    // для подсчёта слов в одном файле
+}
+
+func (wc *WordCounter) ProcessFiles(files ...string) error {
+	var errs error
+	wg := sync.WaitGroup{}
+
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			errs = errors.Join(errs, err)
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			defer f.Close()
+			wc.ProcessReader(f)
+		}()
+	}
+
+	wg.Wait()
+	return errs
+}
+
+func (wc *WordCounter) ProcessReader(r io.Reader) error {
+	scnr := bufio.NewScanner(r)
+	scnr.Split(bufio.ScanWords)
+	for scnr.Scan() {
+		if scnr.Err() != nil {
+			break
+		}
+		wc.wordsCount[strings.ToLower(scnr.Text())]++
+	}
+
+	return scnr.Err()
 }
 
 func main() {

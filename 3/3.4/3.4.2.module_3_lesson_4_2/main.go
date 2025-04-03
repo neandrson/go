6cb935eecae343a9da4a2e16300a708d7066d67d
neandrson <a2361@yandex.ru>
2025-04-03 20:10:28 +0300

    Update main.go

--- 3/3.4/3.4.2.module_3_lesson_4_2/main.go
+++ 3/3.4/3.4.2.module_3_lesson_4_2/main.go
@@ -2,94 +2,30 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"os"
+	"strconv"
 )
 
 func NumbersGen(filename string) <-chan int {
-	/*out := make(chan int) // канал для записи выходных данных
-	bytes, err := os.ReadFile(filename)
+	f, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
-	}
-	go func() {
-		defer close(out) // закроем канал, когда больше нет данных
-		for {
-			_, err = strconv.Atoi(string(bytes[:]))
-			if err == nil {
-				out <- n // запишем в канал
-			}
-		}
-	}()
-	return out // вернём канал*/
-
-	/*out := make(chan int) // канал для записи выходных данных
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("Ошибка открытия файла: %v\n", err)
 		return nil
 	}
+
+	out := make(chan int)
 	go func() {
-
-		defer file.Close()
-
-		data := bufio.NewReader(file)
-		//lineNumber := 1
-		for data.Read() {
-			var x interface{}
-			switch v := x.(type) {
-			case int:
-				out <- v
-			default:
+		defer f.Close()
+		defer close(out)
+		fs := bufio.NewScanner(f)
+		for fs.Scan() {
+			num, err := strconv.Atoi(fs.Text())
+			if err != nil {
 				continue
 			}
-
-			//lineNumber++
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Ошибка сканирования: %v\n", err)
+			out <- num
 		}
 	}()
-	return out // вернём канал*/
 
-	/*file, err := os.Open(filename)
-	if err != nil {
-		return nil
-	}
-	defer file.Close()
-
-	out := make(chan int)
-	r := bufio.NewReader(file)
-	go func() {
-
-		defer close(out)
-		//data := make([]byte, 64)
-
-		l, s, e := r.ReadLine()
-		for s && e == nil {
-			fmt.Println(string(l))
-			//s, e = Readln(r)
-
-		}
-
-	}()
-	return out*/
-
-	f, err := os.Open("temp/file.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	out := make(chan int)
-	// Чтение файла с ридером
-	wr := bytes.Buffer{}
-	sc := bufio.NewScanner(f)
-	go func() {
-	for sc.Scan() {
-		wr.WriteString(sc.Text())
-	}
-	}
 	return out
 }
 

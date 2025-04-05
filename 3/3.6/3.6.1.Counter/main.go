f016283963f9a956f092c9109e18d8ce0fcf60d0
neandrson <a2361@yandex.ru>
2025-04-05 19:12:32 +0300

    Update main.go

--- 3/3.6/3.6.1.Counter/main.go
+++ 3/3.6/3.6.1.Counter/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync/atomic"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
 
 type Counter struct {
 	value int64
@@ -16,4 +20,33 @@ func (c *Counter) Decrement() {
 
 func (c *Counter) GetValue() int64 {
 	return atomic.LoadInt64(&c.value)
+}
+
+func main() {
+	counter := Counter{}
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Increment()
+		}()
+	}
+
+	for i := 0; i < 500; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Decrement()
+		}()
+	}
+
+	wg.Wait()
+
+	if value := counter.GetValue(); value != 500 {
+		fmt.Printf("Expected counter value: 500, got: %v", value)
+	} else {
+		fmt.Printf(" value: %v", value)
+	}
 }

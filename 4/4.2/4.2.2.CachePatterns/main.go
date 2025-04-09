31b02bc3a428cebb6cc5bd14f8f440815452dff0
neandrson <a2361@yandex.ru>
2025-04-09 21:39:07 +0300

    Create main.go

---
+++ 4/4.2/4.2.2.CachePatterns/main.go
@@ -0,0 +1,20 @@
+package main
+
+// структура MRU кеша
+type MRUCache struct {
+}
+
+// возвращает новый инстанс кеша размером capacity
+func NewMRUCache(capacity int) *MRUCache {
+
+}
+
+// устанавливает значени value ключу key
+func (c *MRUCache) Set(key, value string) {
+
+}
+
+// получает значение и флаг его начличия по ключу key
+func (c *MRUCache) Get(key string) (string, bool) {
+
+}
